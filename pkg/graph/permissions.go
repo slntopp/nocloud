3127ca95358ca654033ec698bd946e491110de57
Mik <info@slnt-opp.xyz>
2021-10-13 15:27:12 +0200

    Access - Permissions Graph Edge type

---
+++ pkg/graph/permissions.go
@@ -0,0 +1,9 @@
+package graph
+
+import "github.com/arangodb/go-driver"
+
+type Access struct {
+	From driver.DocumentID `json:"_from"`
+	To driver.DocumentID `json:"_to"`
+	Level int8 `json:"level"`
+}
