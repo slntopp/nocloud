8fc0d68119933eac9380a007b30ad9b800b922c4
Mik <info@slnt-opp.xyz>
2021-10-15 16:54:38 +0200

    has access and access level methods

--- pkg/graph/permissions.go
+++ pkg/graph/permissions.go
@@ -1,6 +1,10 @@
 package graph
 
-import "github.com/arangodb/go-driver"
+import (
+	"context"
+
+	"github.com/arangodb/go-driver"
+)
 
 type Access struct {
 	From driver.DocumentID `json:"_from"`
@@ -8,4 +12,40 @@ type Access struct {
 	Level int8 `json:"level"`
 
 	driver.DocumentMeta
+}
+
+func HasAccess(ctx context.Context, db driver.Database, account string, node string, level int) (bool) {
+	if account == node {
+		return true
+	}
+	_, r := AccessLevel(ctx, db, account, node)
+	return r >= level
+}
+
+func AccessLevel(ctx context.Context, db driver.Database, account string, node string) (bool, int) {
+	query := `FOR path IN OUTBOUND K_SHORTEST_PATHS @account TO @node GRAPH @permissions RETURN path.edges[0].level`
+	c, err := db.Query(ctx, query, map[string]interface{}{
+		"account": account,
+		"node": node,
+		"permissions": "Permissions",
+	})
+	if err != nil {
+		return false, 0
+	}
+	defer c.Close()
+
+	access := 0
+	for {
+		var level int 
+		_, err := c.ReadDocument(ctx, &level)
+		if driver.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			continue
+		}
+		if level > access {
+			access = level
+		}
+	}
+	return access > 0, access
 }
