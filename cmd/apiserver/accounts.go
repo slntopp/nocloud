4e43c96105067f9d5372ef5a588052bc14614788
Mik <info@slnt-opp.xyz>
2021-10-17 04:04:48 +0200

    create account method

--- cmd/apiserver/accounts.go
+++ cmd/apiserver/accounts.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/slntopp/nocloud/pkg/accounts/accountspb"
 	"github.com/slntopp/nocloud/pkg/api/apipb"
+	"github.com/slntopp/nocloud/pkg/nocloud"
+	"go.uber.org/zap"
 )
 
 type accountsAPI struct {
@@ -18,4 +20,10 @@ func (acc *accountsAPI) mustEmbedUnimplementedAccountsServiceServer() {
 
 func (acc *accountsAPI) Token(ctx context.Context, request *accountspb.TokenRequest) (*accountspb.TokenResponse, error) {
 	return acc.client.Token(ctx, request)
+}
+
+func (acc *accountsAPI) Create(ctx context.Context, request *accountspb.CreateRequest) (*accountspb.CreateResponse, error) {
+	log.Debug("context", zap.Any("context", ctx), zap.String("account", ctx.Value(nocloud.NoCloudAccount).(string)))
+	ctx = AddCrossServiceMetadata(ctx)
+	return acc.client.Create(ctx, request)
 }
