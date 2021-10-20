b7b1a7e723ac37b11a1b893ff2d0930553bc8182
Mik <info@slnt-opp.xyz>
2021-10-20 17:19:28 +0200

    fix create graphs while applying scheme

--- database/dump_schemes.sh
+++ database/dump_schemes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-arangodump --server.database nocloud --overwrite --dump-data false --server.password "$ARANGO_ROOT_PASSWORD" --include-system-collections true
+arangodump --server.database nocloud --overwrite --server.password "$ARANGO_ROOT_PASSWORD" --include-system-collections true
 
