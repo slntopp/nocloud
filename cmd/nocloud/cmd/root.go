b24f8f74df21c97478fb07e9ba69b9ea6776ca76
Mik <info@slnt-opp.xyz>
2021-10-21 17:20:16 +0200

    login command base

--- cmd/nocloud/cmd/root.go
+++ cmd/nocloud/cmd/root.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -27,8 +28,8 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "newApp",
-	Short: "A brief description of your application",
+	Use:   "nocloud",
+	Short: "Complete NoCloud client CLI",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
@@ -53,7 +54,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.newApp.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.nocloud.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -70,10 +71,10 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".newApp" (without extension).
+		// Search config in home directory with name ".nocloud" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".newApp")
+		viper.SetConfigName(".nocloud")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
