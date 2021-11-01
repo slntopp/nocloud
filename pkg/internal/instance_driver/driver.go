00c27a0eb4a4928710047e9fbb4c5ce8ceb67640
Mik <info@slnt-opp.xyz>
2021-11-02 00:26:15 +0100

    Basic Driver: API proto

---
+++ pkg/internal/instance_driver/driver.go
@@ -0,0 +1,25 @@
+package driver
+
+import (
+	"context"
+
+	pb "github.com/slntopp/nocloud/pkg/internal/instance_driver/proto"
+)
+
+var DRIVER_NAME string
+
+func SetDriverName(name string) {
+	DRIVER_NAME = name
+}
+
+type DriverServiceServer struct {
+	pb.UnimplementedDriverServiceServer
+}
+
+func (s *DriverServiceServer) GetType(ctx context.Context, _ *pb.GetTypeRequest) (*pb.GetTypeResponse, error) {
+	return &pb.GetTypeResponse{Type: DRIVER_NAME}, nil
+}
+
+func (s *DriverServiceServer) ValidateConfigSyntax(ctx context.Context, req *pb.ValidateConfigSyntaxRequest) (*pb.ValidateConfigSyntaxResponse, error) {
+	return &pb.ValidateConfigSyntaxResponse{Result: true}, nil
+}
