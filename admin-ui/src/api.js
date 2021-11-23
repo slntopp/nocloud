465baeb9579e24ffaf40f1aa503750088b9f240c
Mik <info@slnt-opp.xyz>
2021-11-23 22:12:08 +0100

    remove proxy

--- admin-ui/src/api.js
+++ admin-ui/src/api.js
@@ -1,3 +1,2 @@
 import Api from 'nocloudjsrest';
-// export default new Api("http://localhost/", 8000);
-export default new Api("http://localhost/http://localhost:8000/", 8624);
+export default new Api("http://localhost/", 8000);
