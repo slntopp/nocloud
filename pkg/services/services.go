0bafea9993ecc66392e95d2082faf2045e66256d
Mik <info@slnt-opp.xyz>
2021-11-29 18:04:48 +0100

    fix: use document ID

--- pkg/services/services.go
+++ pkg/services/services.go
@@ -243,6 +243,7 @@ func (s *ServicesServiceServer) Down(ctx context.Context, request *servicespb.Do
 	}
 	log.Debug("Found Service", zap.Any("service", service))
 
+	provisions, err := s.ctrl.GetProvisions(ctx, service.ID.String())
 	if err != nil {
 		s.log.Debug("Can't get provisions for service", zap.Any("service", service))
 	}
