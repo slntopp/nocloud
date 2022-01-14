4ebb3b65512d185f2c6df6192d9e55a1d210fb6a
Mik <info@slnt-opp.xyz>
2022-01-14 14:19:02 +0100

    fix: api route cleanup

--- admin-ui/src/api.js
+++ admin-ui/src/api.js
@@ -1,7 +1,7 @@
 import Api from 'nocloudjsrest';
 import vuex from '@/store/index.js'
-// const api = new Api();
-const api = new Api("http://localhost/https://rest.nocloud.ione-cloud.net/", 8624);
+const api = new Api();
+// const api = new Api("http://localhost/https://rest.nocloud.ione-cloud.net/", 8624);
 
 api.axios.interceptors.response.use(function (response) {
   return response;
