39dbee3247393ea0198ef6bd5b37a646730ca519
Alexandr Korolev <dowellkin@gmail.com>
2022-01-28 16:04:48 +0300

    service create page done

--- admin-ui/src/components/service-create.vue
+++ admin-ui/src/components/service-create.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-form v-model='formValid'>
+	<v-form v-model='formValid' ref="form">
 		<v-row
 			justify="start"
 		>
@@ -19,8 +19,24 @@
 				md="4"
 				lg="3"
 			>
+				<v-select
+					label="namespace"
+					:rules="rules.req"
+					:items="namespaces"
+					:loading="namespacesLoading"
+					item-text="title"
+					item-value="uuid"
+					v-model="namespace"
+				>	
+				</v-select>
+			</v-col>
+			<v-col
+				cols="6"
+				md="4"
+				lg="3"
+			>
 				<v-text-field
-					label="service title"
+					label="version"
 					:rules="rules.req"
 					v-model="service.version"
 					readonly
@@ -38,12 +54,12 @@
 					dence
 					color="background-light"
 				>
-					<v-subheader>instances {{currentInstancesGroupsIndex}}</v-subheader>
+					<v-subheader>instances groups</v-subheader>
 
 					<v-list-item
 						v-for="(instance, index) in instances"
 						:key="index"
-						@click="() => selectIntance(index)"
+						@click="() => selectInstance(index)"
 
 						:class="{ 'v-list-item--active': index == currentInstancesGroupsIndex }"
 					>
@@ -55,15 +71,37 @@
 						</v-list-item-content>
 					</v-list-item>
 
-					<v-list-item @click="() => addInstance()">
+					<v-list-item @click="() => addInstancesGroup()">
 						<v-list-item-icon>
 							<v-icon>mdi-plus-circle-outline</v-icon>
 						</v-list-item-icon>
 						<v-list-item-content>
-							<v-list-item-title>add instance</v-list-item-title>
+							<v-list-item-title>add instances group</v-list-item-title>
 						</v-list-item-content>
 					</v-list-item>
 				</v-list>
+
+				<div class="btns__wrapper d-flex justify-end mt-4">
+					<router-link
+						:to="{ name: 'Services' }"
+						style="text-decoration: none"
+					>
+						<v-btn>cancel</v-btn>
+					</router-link>
+					<v-btn
+						class="ml-2"
+						@click="testService"
+					>
+						test
+					</v-btn>
+					<v-btn
+						class="ml-2"
+						@click="createService"
+						:disabled="!testsPassed"
+					>
+						create
+					</v-btn>
+				</div>
 			</v-col>
 			<v-col
 				cols="8"
@@ -80,9 +118,10 @@
 					<v-btn class="mb-4" @click="() => removeInstance(currentInstancesGroupsIndex)">Remove</v-btn>
 					
 					<v-text-field
-						label="instance title"
+						label="instances group title"
 						:rules="rules.req"
 						v-model="instances[currentInstancesGroupsIndex].title"
+						@change="(newVal) => currentInstancesGroups.title = newVal"
 					/>
 
 					<v-select
@@ -110,10 +149,40 @@
 				</v-card>
 			</v-col>
 		</v-row>
+
+		
+
+		<v-snackbar
+			v-model="snackbar.visibility"
+			:timeout="snackbar.timeout"
+			:color="snackbar.color"
+		>
+			{{snackbar.message}}
+			<template v-if="snackbar.route && Object.keys(snackbar.route).length > 0">
+				<router-link :to="snackbar.route">
+					Look up.
+				</router-link>
+			</template>
+			
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          :color="snackbar.buttonColor"
+          text
+          v-bind="attrs"
+          @click="snackbar.visibility = false"
+        >
+          Close
+        </v-btn>
+      </template>
+		</v-snackbar>
 	</v-form>
 </template>
 
 <script>
+import api from "@/api"
+import snackbar from "@/mixins/snackbar.js"
+
 export default {
 	name: 'service-create',
 	data: () => ({
@@ -137,6 +206,7 @@ export default {
 			"instances_groups": {
 			}
 		},
+		namespace: "",
 		instances: [],
 		currentInstancesGroups: {},
 		currentInstancesGroupsIndex: -1,
@@ -144,23 +214,24 @@ export default {
 			'ione',
 			'custom'
 		],
-		templates: {}
+		templates: {},
+
+		testsPassed: false
 	}),
+	mixins: [snackbar],
 	methods: {
-		addInstance(title = ""){
+		addInstancesGroup(title = ""){
 			if(this.instances.some(inst => inst.title == ''))
 				return
 			this.instances = [...this.instances, this.defaultInstance(title)]
 			if(this.instances.length == 1)
-				this.selectIntance(0)
+				this.selectInstance(0)
 		},
 		removeInstance(index){
 			this.instances.splice(index, 1)
-			let newIndex = index - 1;
-			if(this.instances.length > 0 && newIndex == -1)
-				newIndex = 0
+			let newIndex = Math.max(index - 1, 0);
 				
-			this.selectIntance(newIndex)
+			this.selectInstance(newIndex)
 		},
 		defaultInstance(title = ""){
 			return {
@@ -168,13 +239,13 @@ export default {
 				body: {
 					type: 'ione',
 					"resources": {
-							"ips_public": 1
+						"ips_public": 1
 					},
 				}
 			}
 		},
-		selectIntance(index = -1){
-			if(index >= 0){
+		selectInstance(index = -1){
+			if(this.instances.length > 0){
 				this.currentInstancesGroups = JSON.parse(JSON.stringify(this.instances[index]))
 			} else {
 				this.currentInstancesGroups = {}
@@ -183,10 +254,70 @@ export default {
 		},
 		receiveObject(newVal){
 			this.instances[this.currentInstancesGroupsIndex] = JSON.parse(newVal);
-			this.selectIntance(this.currentInstancesGroupsIndex)
-		}
+			this.selectInstance(this.currentInstancesGroupsIndex)
+			this.testsPassed = false
+		},
+		getService(){
+			const data = JSON.parse(JSON.stringify(this.service));
+			const instances = JSON.parse(JSON.stringify(this.instances));
+
+			instances.forEach(inst => {
+				data.instances_groups[inst.title] = inst.body
+			});
+			return {namespace: this.namespace, service: data};
+		},
+		createService(){
+			const data = this.getService();
+			
+			if(!this.formValid){
+				this.$refs.form.validate()
+				return
+			}
+
+			api.services.create(data)
+			.then(() => {
+				this.showSnackbar({message: `Service created successfully`})
+				this.$router.push( {name: 'Services'} )
+			})
+			.catch(err => {
+				const opts = {
+					message: `Error: ${err?.response?.data?.message ?? 'Unknown'}.`,
+				}
+				this.showSnackbarError(opts);
+			})
+		},
+		testService(){
+			const data = this.getService();
+			
+			if(!this.formValid){
+				this.$refs.form.validate()
+				return
+			}
+
+			api.services.testConfig(data)
+			.then(() => {
+				this.showSnackbar({message: `Service passed tests successfully`})
+				this.testsPassed = true;
+			})
+			.catch(err => {
+				this.testsPassed = false;
+				const opts = {
+					message: `Error: ${err?.response?.data?.message ?? 'Unknown'}.`,
+				}
+				this.showSnackbarError(opts);
+			})
+		},
+	},
+	computed: {
+		namespaces(){
+			return this.$store.getters['namespaces/all'];
+		},
+		namespacesLoading(){
+			return this.$store.getters['namespaces/isLoading'];
+		},
 	},
 	created(){
+		this.$store.dispatch('namespaces/fetch')
 		const types = require.context('@/components/modules/', true, /serviceCreate\.vue$/)
 		types.keys().forEach(key => {
 			const matched = key.match(/\.\/([A-Za-z0-9-_,\s]*)\/serviceCreate\.vue/i);
@@ -196,9 +327,16 @@ export default {
 				this.templates[type] = () => import(`@/components/modules/${type}/serviceCreate.vue`)
 			}
 		})
-
-		this.addInstance('test')
+	},
+	watch: {
+		service: {
+			handler(){
+				this.testsPassed = false
+			},
+			deep: true
+		}
 	}
+	
 }
 </script>
 
