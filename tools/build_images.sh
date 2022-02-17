954c21250a7c88033b06d7693b9b40f57876371b
Mik <info@slnt-opp.xyz>
2022-02-17 13:31:12 +0100

    add base image to build scripts

--- tools/build_images.sh
+++ tools/build_images.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+docker build . -t ghcr.io/slntopp/nocloud/base:latest
+
 for image in Dockerfiles/*; do
     tag="ghcr.io/slntopp/nocloud/$(basename $image):latest"
     docker build . -f "$image/Dockerfile" -t $tag
