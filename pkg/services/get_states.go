2de9a354f967de42baaf5680dc988a55d2570788
Artyom <user@mail.com>
2022-02-25 17:54:13 +0700

    driver.proto updated

--- pkg/services/get_states.go
+++ pkg/services/get_states.go
@@ -18,6 +18,8 @@ package services
 import (
 	"context"
 
+	driverpb "github.com/slntopp/nocloud/pkg/drivers/instance/vanilla"
+	instances "github.com/slntopp/nocloud/pkg/instances/proto"
 	pb "github.com/slntopp/nocloud/pkg/services/proto"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
@@ -42,4 +44,62 @@ func (s *ServicesServiceServer) GetStates(ctx context.Context, request *pb.GetSt
 	}
 
 	return resp, nil
+}
+
+//Gets statuses Instanses of Servce from pkg/statuses
+func (s *ServicesServiceServer) UpdateStates(ctx context.Context, request *pb.GetStatesRequest) error {
+	log := s.log.Named("UpdateStates")
+
+	// s.ticker.Stop()
+	//s.ticker.Reset()
+
+	for range s.ticker.C {
+
+		var ig_array []*instances.InstancesGroup
+
+		sp_array, err := s.sp_ctrl.List(ctx, "") // gets service providers
+		if err != nil {
+			log.Error("fail to get ServicesProviders", zap.Error(err))
+			return status.Error(codes.Internal, "fail to get ServicesProviders")
+		}
+
+		for isp := range sp_array {
+
+			sp := sp_array[isp]
+			s_array, err := s.sp_ctrl.ListDeployments(ctx, sp) // gets services
+			if err != nil {
+				log.Error("fail to get Services", zap.Error(err))
+				return status.Error(codes.Internal, "fail to get Services")
+				// continue? todo
+			}
+
+			for is := range s_array {
+				g_array := s_array[is].GetInstancesGroups() // gets groups
+				for _, ig := range g_array {
+					ig_array = append(ig_array, ig)
+				}
+
+			}
+
+			// 1. pool = List ServicesProviders
+			// 2. for sp in pool {
+			//    groups := sp.ListDeployments()
+			//    driver.MonitorStates({sp, groups})
+			// }
+
+			_, err = s.drivers["key"].MonitorStates(ctx, &driverpb.StateUpdateRequest{//key todo
+				Group:            ig_array,
+				ServicesProvider: sp.ServicesProvider,
+			})
+			if err != nil {
+				log.Error("fail to UpdateStates", zap.Error(err))
+				return status.Error(codes.Internal, "fail to UpdateStates")
+				// continue? todo
+			}
+
+		}
+
+	}
+
+	return nil
 }
