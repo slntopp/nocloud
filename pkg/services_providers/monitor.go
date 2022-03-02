e6444c9fb64ac42fc938909dadb9b5d6ae807f9e
Mik <info@slnt-opp.xyz>
2022-03-02 01:05:40 +0100

    handle ticker frequency from settings and settings sub

--- pkg/services_providers/monitor.go
+++ pkg/services_providers/monitor.go
@@ -17,19 +17,120 @@ package services_providers
 
 import (
 	"context"
+	"encoding/json"
+	"time"
 
 	driverpb "github.com/slntopp/nocloud/pkg/drivers/instance/vanilla"
 	instpb "github.com/slntopp/nocloud/pkg/instances/proto"
+	settingspb "github.com/slntopp/nocloud/pkg/settings/proto"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/slntopp/nocloud/pkg/graph"
 	"github.com/slntopp/nocloud/pkg/nocloud/schema"
 	"go.uber.org/zap"
 )
 
+var (
+	settingsClient settingspb.SettingsServiceClient
+)
+
+// Settings Key storing monitoring routine conf
+const monFreqKey string = "sp-monitoring-routine"
+
+type MonitoringRoutineConf struct {
+	Frequency int `json:"freq"` // Frequency in Seconds
+}
+
+var (
+	defaultConf = MonitoringRoutineConf{
+		Frequency: 60,
+	}
+
+	description = "ServicesProviders Monitoring Routing Configuration"
+	public = false
+)
+
+func init() {
+	viper.AutomaticEnv()
+	viper.SetDefault("SETTINGS_HOST", "settings:8080")
+    host := viper.GetString("SETTINGS_HOST")
+    
+	conn, err := grpc.Dial(host, grpc.WithTransportCredentials(insecure.NewCredentials()))
+    if err != nil {
+        panic(err)
+    }
+
+    settingsClient = settingspb.NewSettingsServiceClient(conn)
+}
+
+func MakeConf(ctx context.Context) MonitoringRoutineConf {
+
+	var conf MonitoringRoutineConf
+	var r_str string
+	r, err := settingsClient.Get(ctx, &settingspb.GetRequest{Keys: []string{monFreqKey}})
+	if err != nil {
+		goto set_default
+	}
+	if _, ok := r.GetFields()[monFreqKey]; !ok {
+		goto set_default
+	}
+	r_str = r.GetFields()[monFreqKey].GetStringValue()
+	err = json.Unmarshal([]byte(r_str), &conf)
+	if err != nil {
+		goto set_default
+	}
+	return conf
+
+	set_default:
+	conf = defaultConf
+	payload, err := json.Marshal(conf)
+	if err == nil {
+		settingsClient.Put(ctx, &settingspb.PutRequest{
+			Key: monFreqKey,
+			Value: string(payload),
+			Description: &description,
+			Public: &public,
+		})
+	}
+	return conf
+}
+
 func (s *ServicesProviderServer) MonitoringRoutine(ctx context.Context) {
 	log := s.log.Named("MonitoringRoutine")
 
-	for tick := range s.ticker.C {
+	conf := MakeConf(ctx)
+	ticker := time.NewTicker(time.Second * time.Duration(conf.Frequency))
+
+	go func ()  {
+		begin_sub:
+		sub, err := settingsClient.Sub(ctx, &settingspb.SubRequest{Key: "sp-monitoring-routing"})
+		if err != nil {
+			log.Error("Could not subscribe to Settings update", zap.Error(err))
+			return
+		}
+
+		for {
+			data, err := sub.Recv()
+			if err != nil {
+				log.Error("Error reading from Settings Sub channel", zap.Error(err))
+				break
+			}
+			err = json.Unmarshal([]byte(data.GetValue()), &conf)
+			if err != nil {
+				log.Error("Error unmarshall udpate from Settings Sub channel", zap.Error(err))
+				break
+			}
+			ticker.Reset(time.Duration(conf.Frequency))
+		}
+
+		log.Info("Monitoring Conf Updated", zap.Any("conf", conf))
+		time.Sleep(time.Duration(conf.Frequency))
+		goto begin_sub
+	}()
+
+	for tick := range ticker.C {
 		log.Info("Starting", zap.Time("tick", tick))
 
 		sp_pool, err := s.ctrl.List(ctx, schema.ROOT_ACCOUNT_KEY)
