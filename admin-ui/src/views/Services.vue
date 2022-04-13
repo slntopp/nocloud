f5ed45e427470a09afb928cec12fb1d69fdd55c0
va-ovsyanikov <va.ovsyanikov@gmail.com>
2022-04-13 18:32:08 +0300

    sp:custom key

--- admin-ui/src/views/Services.vue
+++ admin-ui/src/views/Services.vue
@@ -76,7 +76,7 @@
                     v-for="(elem, index) in group.instances"
                     :key="index"
                     :title="elem.title"
-                    :state="elem.state ? elem.state.state : 'UNKNOWN' "
+                    :state="elem.state ? elem.state.state : 'UNKNOWN'"
                     :cpu="elem.resources.cpu"
                     :drive_type="elem.resources.drive_type"
                     :drive_size="elem.resources.drive_size"
