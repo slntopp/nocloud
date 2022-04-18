1e39c59a6fbffddd9efbe721fea0d73ddd706af9
Mik <info@slnt-opp.xyz>
2022-04-18 23:15:40 +0200

    rm database stored schemas

--- database/dump_schemes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-arangodump --server.database nocloud --overwrite --server.password "$ARANGO_ROOT_PASSWORD" --include-system-collections true
-cp /dump/* /docker-entrypoint-initdb.d/dumps/nocloud
