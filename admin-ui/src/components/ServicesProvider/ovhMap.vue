cdee6e6080109a9bbefb9b219908be7e57f5c55e
PavelSazukin <sazukinpavelal@gmail.com>
2022-09-26 20:22:13 +0300

    add spinner

--- admin-ui/src/components/ServicesProvider/ovhMap.vue
+++ admin-ui/src/components/ServicesProvider/ovhMap.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <v-list flat dark color="rgba(12, 12, 60, 0.9)">
+    <v-progress-circular v-if="!allRegions.length" class="spinner" size="40" color="primary" indeterminate/>
+    <v-list v-else flat dark color="rgba(12, 12, 60, 0.9)">
       <v-list-item-group v-model="selectedRegion" color="primary">
         <v-list-item v-for="(item, i) in allRegions" :key="i">
           {{ item }}
@@ -57,5 +58,9 @@ export default {
   display: grid;
   grid-template-columns: 100px 1fr;
   grid-column-gap: 20px;
+}
+.spinner{
+  margin: auto;
+  margin-top: 150px;
 }
 </style>
