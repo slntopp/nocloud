e21eb0a2e9ec4044dcec4c2027639c2889af66f5
PavelSazukin <sazukinpavelal@gmail.com>
2022-09-30 17:51:02 +0300

    add search by title in accounts

--- admin-ui/src/components/accounts_table.vue
+++ admin-ui/src/components/accounts_table.vue
@@ -1,96 +1,105 @@
 <template>
-	<nocloud-table
+  <nocloud-table
     :headers="headers"
-		:items="tableData"
-		:value="selected"
-		:loading="loading"
-		:single-select="singleSelect"
-		:footer-error="fetchError"
-		@input="handleSelect"
-	>
+    :items="filtredAccounts"
+    :value="selected"
+    :loading="loading"
+    :single-select="singleSelect"
+    :footer-error="fetchError"
+    @input="handleSelect"
+  >
     <template v-slot:[`item.title`]="{ item }">
-      <router-link
-        :to="{ name: 'Account', params: { accountId: item.uuid } }"
-      >
+      <router-link :to="{ name: 'Account', params: { accountId: item.uuid } }">
         {{ item.title }}
       </router-link>
     </template>
-    <template v-slot:[`item.balance`]='{ item }'>
+    <template v-slot:[`item.balance`]="{ item }">
       <balance v-if="item.balance" :value="item.balance" />
       <template v-else>-</template>
     </template>
-	</nocloud-table>
+  </nocloud-table>
 </template>
 
 <script>
-import noCloudTable from "@/components/table.vue"
-import Balance from './balance.vue'
+import noCloudTable from "@/components/table.vue";
+import Balance from "./balance.vue";
 
 export default {
-	name: "accounts-table",
-	components: {
-		"nocloud-table": noCloudTable,
-    Balance
-	},
-	props: {
-		value: {
-			type: Array,
-			default: () => []
-		},
-		"single-select": {
-			type: Boolean,
-			default: false
-		}
-	},
-	data () {
-		return {
-			selected: this.value,
-			loading: false,
-			fetchError: '',
+  name: "accounts-table",
+  components: {
+    "nocloud-table": noCloudTable,
+    Balance,
+  },
+  props: {
+    value: {
+      type: Array,
+      default: () => [],
+    },
+    "single-select": {
+      type: Boolean,
+      default: false,
+    },
+    searchParam: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      selected: this.value,
+      loading: false,
+      fetchError: "",
       headers: [
-        { text: 'Title', value: 'title' },
-        { text: 'UUID', value: 'uuid' },
-        { text: 'Balance', value: 'balance' }
-      ]
-		}
-	},
-	methods: {
-		handleSelect(item){
-			this.$emit('input', item)
-		}
-	},
-	computed: {
-		tableData(){
-			return this.$store.getters['accounts/all'];
-		}
-	},
-	created() {
-		this.loading = true;
-		this.$store.dispatch('accounts/fetch')
-		.then(() => {
-			this.fetchError = ''
-		})
-		.finally(()=>{
-			this.loading = false;
-		})
-		.catch(err => {
-			console.error(err.toJSON())
-			this.fetchError = 'Can\'t reach the server'
-			if(err.response && err.response.data.message){
-				this.fetchError += `: [ERROR]: ${err.response.data.message}`
-			} else {
-				this.fetchError += `: [ERROR]: ${err.toJSON().message}`
-			}
-		})
-	},
+        { text: "Title", value: "title" },
+        { text: "UUID", value: "uuid" },
+        { text: "Balance", value: "balance" },
+      ],
+    };
+  },
+  methods: {
+    handleSelect(item) {
+      this.$emit("input", item);
+    },
+  },
+  computed: {
+    tableData() {
+      return this.$store.getters["accounts/all"];
+    },
+    filtredAccounts() {
+      if (this.searchParam) {
+        return this.tableData.filter((account) =>
+          account.title.toLowerCase().includes(this.searchParam)
+        );
+      }
+      return this.tableData;
+    },
+  },
+  created() {
+    this.loading = true;
+    this.$store
+      .dispatch("accounts/fetch")
+      .then(() => {
+        this.fetchError = "";
+      })
+      .finally(() => {
+        this.loading = false;
+      })
+      .catch((err) => {
+        console.error(err.toJSON());
+        this.fetchError = "Can't reach the server";
+        if (err.response && err.response.data.message) {
+          this.fetchError += `: [ERROR]: ${err.response.data.message}`;
+        } else {
+          this.fetchError += `: [ERROR]: ${err.toJSON().message}`;
+        }
+      });
+  },
   watch: {
     tableData() {
-      this.fetchError = '';
-    }
-  }
-}
+      this.fetchError = "";
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- admin-ui/src/views/Accounts.vue
+++ admin-ui/src/views/Accounts.vue
@@ -1,106 +1,120 @@
 <template>
   <div class="namespaces pa-4 flex-wrap">
-    <div class="buttons__inline pb-4">
-      <v-menu
-        offset-y
-        transition="slide-y-transition"
-        bottom
-        :close-on-content-click="false"
-        v-model="createMenuVisible"
-      >
-        <template v-slot:activator="{ on, attrs }">
+    <div class="pb-4 buttons">
+      <div class="buttons__inline">
+        <v-menu
+          offset-y
+          transition="slide-y-transition"
+          bottom
+          :close-on-content-click="false"
+          v-model="createMenuVisible"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="background-light"
+              class="mr-2"
+              v-bind="attrs"
+              v-on="on"
+              @click="openCreateAccountMenuHandler"
+            >
+              create
+            </v-btn>
+          </template>
+          <v-card class="pa-4">
+            <v-from ref="form" v-model="newAccount.formValid">
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    dense
+                    v-model="newAccount.data.title"
+                    placeholder="title"
+                    :rules="newAccount.rules.title"
+                  >
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    dense
+                    hide-details
+                    v-model="newAccount.data.auth.data[0]"
+                    placeholder="username"
+                  >
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    dense
+                    hide-details
+                    v-model="newAccount.data.auth.data[1]"
+                    placeholder="password"
+                    type="password"
+                  >
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-select
+                    :items="namespacesForSelect"
+                    v-model="newAccount.data.namespace"
+                    label="namespace"
+                    :rules="newAccount.rules.selector"
+                  ></v-select>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-select
+                    :items="accessLevels"
+                    v-model="newAccount.data.access"
+                    label="access"
+                  ></v-select>
+                </v-col>
+              </v-row>
+              <v-row justify="end">
+                <v-col md="5">
+                  <v-btn :loading="newAccount.loading" @click="createAccount">
+                    send
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-from>
+          </v-card>
+        </v-menu>
+        <confirm-dialog
+          :disabled="selected.length < 1"
+          @confirm="deleteSelectedAccount"
+        >
           <v-btn
             color="background-light"
-            class="mr-2"
-            v-bind="attrs"
-            v-on="on"
-            @click="openCreateAccountMenuHandler"
+            class="mr-8"
+            :disabled="selected.length < 1"
+            :loading="deletingLoading"
           >
-            create
+            delete
           </v-btn>
-        </template>
-        <v-card class="pa-4">
-          <v-from ref="form" v-model="newAccount.formValid">
-            <v-row>
-              <v-col>
-                <v-text-field
-                  dense
-                  v-model="newAccount.data.title"
-                  placeholder="title"
-                  :rules="newAccount.rules.title"
-                >
-                </v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-text-field
-                  dense
-                  hide-details
-                  v-model="newAccount.data.auth.data[0]"
-                  placeholder="username"
-                >
-                </v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-text-field
-                  dense
-                  hide-details
-                  v-model="newAccount.data.auth.data[1]"
-                  placeholder="password"
-                  type="password"
-                >
-                </v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-select
-                  :items="namespacesForSelect"
-                  v-model="newAccount.data.namespace"
-                  label="namespace"
-                  :rules="newAccount.rules.selector"
-                ></v-select>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-select
-                  :items="accessLevels"
-                  v-model="newAccount.data.access"
-                  label="access"
-                ></v-select>
-              </v-col>
-            </v-row>
-            <v-row justify="end">
-              <v-col md="5">
-                <v-btn :loading="newAccount.loading" @click="createAccount">
-                  send
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-from>
-        </v-card>
-      </v-menu>
-
-      <confirm-dialog
-        :disabled="selected.length < 1"
-        @confirm="deleteSelectedAccount"
-      >
-        <v-btn
-          color="background-light"
-          class="mr-8"
-          :disabled="selected.length < 1"
-          :loading="deletingLoading"
-        >
-          delete
-        </v-btn>
-      </confirm-dialog>
+        </confirm-dialog>
+      </div>
+      <div>
+        <v-text-field
+          v-model="searchParam"
+          class="accounts-search mr-2"
+          hide-details
+          prepend-inner-icon="mdi-magnify"
+          placeholder="Search..."
+          background-color="background-light"
+          dence
+          rounded
+        ></v-text-field>
+      </div>
     </div>
 
-    <accounts-table v-model="selected"> </accounts-table>
+    <accounts-table :searchParam="searchParam" v-model="selected">
+    </accounts-table>
 
     <v-snackbar
       v-model="snackbar.visibility"
@@ -127,119 +141,136 @@
 </template>
 
 <script>
-import accountsTable from "@/components/accounts_table.vue"
-import api from "@/api.js"
+import accountsTable from "@/components/accounts_table.vue";
+import api from "@/api.js";
 
-import snackbar from "@/mixins/snackbar.js"
-import ConfirmDialog from '../components/confirmDialog.vue'
+import snackbar from "@/mixins/snackbar.js";
+import ConfirmDialog from "../components/confirmDialog.vue";
 
 export default {
-	name: "accounts-view",
-	components: {
-		"accounts-table": accountsTable,
-ConfirmDialog,
-	},
-	mixins: [snackbar],
-	data () {
-		return {
-			createMenuVisible: false,
-			selected: [],
-			newAccount: {
-				data: {
-					title: '',
-					auth: {
-						type: 'standard',
-						data: [
-							'', ''
-						]
-					},
-					namespace: '',
-					access: 1,
-				},
-				rules: {
-					title: [
-						value => !!value || 'Title is required',
-						value => (value || '').length >= 3 || 'Min 3 characters',
-					],
-					selector: [
-						value => !!value || 'Namespace is required',
-					]
-				},
-				formValid: true,
-				loading: false
-			},
-			deletingLoading: false,
-			accessLevels: [0, 1, 2, 3],
-		}
-	},
-	methods: {
-		createAccount(){
-			if(!this.newAccount.formValid) return;
-			this.newAccount.loading = true;
-			api.accounts.create(this.newAccount.data)
-			.then(()=>{
-				this.newAccount.title = '';
-				this.createMenuVisible = false;
-				this.$store.dispatch('accounts/fetch');
+  name: "accounts-view",
+  components: {
+    "accounts-table": accountsTable,
+    ConfirmDialog,
+  },
+  mixins: [snackbar],
+  data() {
+    return {
+      createMenuVisible: false,
+      selected: [],
+      newAccount: {
+        data: {
+          title: "",
+          auth: {
+            type: "standard",
+            data: ["", ""],
+          },
+          namespace: "",
+          access: 1,
+        },
+        rules: {
+          title: [
+            (value) => !!value || "Title is required",
+            (value) => (value || "").length >= 3 || "Min 3 characters",
+          ],
+          selector: [(value) => !!value || "Namespace is required"],
+        },
+        formValid: true,
+        loading: false,
+      },
+      deletingLoading: false,
+      accessLevels: [0, 1, 2, 3],
+      searchParam: "",
+    };
+  },
+  methods: {
+    createAccount() {
+      if (!this.newAccount.formValid) return;
+      this.newAccount.loading = true;
+      api.accounts
+        .create(this.newAccount.data)
+        .then(() => {
+          this.newAccount.title = "";
+          this.createMenuVisible = false;
+          this.$store.dispatch("accounts/fetch");
 
-				this.newAccount.data = {
-					title: '',
-					auth: {
-						type: 'standard',
-						data: [
-							'', ''
-						]
-					},
-					namespace: '',
-					access: 1,
-				}
-			})
-			.catch((error)=>{
-				console.error(error)
-				this.snackbar.message = "Something went wrong... Try later."
-				this.snackbar.visibility = true;
-			})
-			.finally(()=>{
-				this.newAccount.loading = false;
-			})
-		},
-		deleteSelectedAccount(){
-			if(this.selected.length > 0){
-				const deletePromices = this.selected.map(el => api.accounts.delete(el.uuid));
-				this.deletingLoading = true;
-				Promise.all(deletePromices)
-				.then(res => {
-					if(res.every(el => el.result)){
-						this.snackbar.message = `Account${deletePromices.length == 1 ? "" : "s"} deleted successfully.`
-						this.snackbar.visibility = true;
-					}
+          this.newAccount.data = {
+            title: "",
+            auth: {
+              type: "standard",
+              data: ["", ""],
+            },
+            namespace: "",
+            access: 1,
+          };
+        })
+        .catch((error) => {
+          console.error(error);
+          this.snackbar.message = "Something went wrong... Try later.";
+          this.snackbar.visibility = true;
+        })
+        .finally(() => {
+          this.newAccount.loading = false;
+        });
+    },
+    deleteSelectedAccount() {
+      if (this.selected.length > 0) {
+        const deletePromices = this.selected.map((el) =>
+          api.accounts.delete(el.uuid)
+        );
+        this.deletingLoading = true;
+        Promise.all(deletePromices)
+          .then((res) => {
+            if (res.every((el) => el.result)) {
+              this.snackbar.message = `Account${
+                deletePromices.length == 1 ? "" : "s"
+              } deleted successfully.`;
+              this.snackbar.visibility = true;
+            }
 
-					this.selected = [];
-					this.$store.dispatch('accounts/fetch');
-				})
-				.catch(err => {
-					console.error(err);
-				})
-				.finally(() => {
-					this.deletingLoading = false;
-				})
-			}
-		},
-		openCreateAccountMenuHandler(){
-			this.$store.dispatch('namespaces/fetch');
-		}
-	},
-	computed: {
-		namespacesForSelect(){
-			let namespaces = this.$store.getters['namespaces/all'] ?? [];
-			namespaces = namespaces.map(namespace => ({text: namespace.title, value: namespace.uuid}))
-			return namespaces
-		}
-	},
-	mounted(){
-		this.$store.commit('reloadBtn/setCallback', {func: this.$store.dispatch, params: ['accounts/fetch']})
-	}
-}
+            this.selected = [];
+            this.$store.dispatch("accounts/fetch");
+          })
+          .catch((err) => {
+            console.error(err);
+          })
+          .finally(() => {
+            this.deletingLoading = false;
+          });
+      }
+    },
+    openCreateAccountMenuHandler() {
+      this.$store.dispatch("namespaces/fetch");
+    },
+  },
+  computed: {
+    namespacesForSelect() {
+      let namespaces = this.$store.getters["namespaces/all"] ?? [];
+      namespaces = namespaces.map((namespace) => ({
+        text: namespace.title,
+        value: namespace.uuid,
+      }));
+      return namespaces;
+    },
+  },
+  mounted() {
+    this.$store.commit("reloadBtn/setCallback", {
+      func: this.$store.dispatch,
+      params: ["accounts/fetch"],
+    });
+  },
+};
 </script>
 
-<style></style>
+<style>
+.buttons {
+  display: flex;
+  justify-content: space-between;
+}
+
+.accounts-search {
+  margin-top: 0px;
+  font-size: 1.2rem;
+  padding-top: 0px;
+}
+</style>
