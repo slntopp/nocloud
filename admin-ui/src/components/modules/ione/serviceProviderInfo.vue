2d298fe77f6567b52bc662ebe11e360bb4df6269
PavelSazukin <sazukinpavelal@gmail.com>
2022-10-13 14:51:25 +0300

    fix vlans panel

--- admin-ui/src/components/modules/ione/serviceProviderInfo.vue
+++ admin-ui/src/components/modules/ione/serviceProviderInfo.vue
@@ -295,7 +295,14 @@
             :key="OS.name"
           >
             <div class="newCloud__template-image">
-              <img :src="'img/OS/' + OS.name.replace(/[^a-zA-Z]+/g, '').toLowerCase() + '.png'" :alt="OS.name" />
+              <img
+                :src="
+                  'img/OS/' +
+                  OS.name.replace(/[^a-zA-Z]+/g, '').toLowerCase() +
+                  '.png'
+                "
+                :alt="OS.name"
+              />
             </div>
             <div class="newCloud__template-name">
               {{ OS.name }}
@@ -320,7 +327,9 @@
           <v-btn class="mr-2" v-if="counter > 1" @click="counter--">
             less
           </v-btn>
-          <v-btn v-if="counter < 8" @click="counter++"> more </v-btn>
+          <v-btn v-if="counter < vlansRowCount" @click="counter++">
+            more
+          </v-btn>
         </div>
       </v-col>
     </v-row>
@@ -352,16 +361,16 @@ export default {
         }
       }
     },
-    getVlanIndex(index){
-      return this.vlansStart? this.vlansStart+index:index
-    }
+    getVlanIndex(index) {
+      return this.vlansStart ? this.vlansStart + index : index;
+    },
   },
   mounted() {
     this.changeWidth();
   },
   computed: {
-    vlansKey(){
-      return Object.keys(this.template.secrets.vlans)[0]
+    vlansKey() {
+      return Object.keys(this.template.secrets.vlans)[0];
     },
     vlans() {
       const { free_vlans } = this.template?.state.meta.networking.private_vnet;
@@ -371,19 +380,31 @@ export default {
         vlans += +value;
       });
 
-      const oneBlockLength=this.vlansCount/8
-
-      const res = Array.from({ length: oneBlockLength * this.counter+1 })
+      const res = Array.from({
+        length: (this.vlansCount / this.vlansRowCount) * this.counter + 1,
+      })
         .fill(1, 0, vlans)
         .fill(0, vlans);
       return res;
     },
-    vlansCount(){
-      return this.template.secrets.vlans[this.vlansKey]?.size
+    vlansCount() {
+      return this.template.secrets.vlans[this.vlansKey]?.size;
     },
-    vlansStart(){
-      return this.template.secrets.vlans[this.vlansKey]?.start
-    }
+    vlansStart() {
+      return this.template.secrets.vlans[this.vlansKey]?.start;
+    },
+    vlansRowCount() {
+      if (this.vlansCount < 300) {
+        return 1;
+      }
+      if (this.vlansCount < 1000) {
+        return 4;
+      }
+      if (this.vlansCount < 2000) {
+        return 6;
+      }
+      return 8;
+    },
   },
   watch: {
     counter() {
@@ -420,7 +441,7 @@ export default {
   &:not(:last-child) {
     margin-right: 10px;
   }
-   &:first-child {
+  &:first-child {
     margin-left: 0px;
   }
   &:hover {
@@ -429,17 +450,16 @@ export default {
   }
 }
 
-  .newCloud__template-image {
-    padding: 10px;
-  }
-  .newCloud__template-image img {
-    margin: auto;
-    width: 90%;
-    height: 100%;
-  }
+.newCloud__template-image {
+  padding: 10px;
+}
+.newCloud__template-image img {
+  margin: auto;
+  width: 90%;
+  height: 100%;
+}
 
 .newCloud__template-name {
   padding: 10px;
 }
-
 </style>
