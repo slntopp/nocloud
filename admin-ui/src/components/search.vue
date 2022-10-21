9afe9aeba05bdb496da3b921760b44491822acb2
PavelSazukin <sazukinpavelal@gmail.com>
2022-10-21 16:43:27 +0300

    restructurize search

---
+++ admin-ui/src/components/search.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-text-field
+    hide-details
+    prepend-inner-icon="mdi-magnify"
+    placeholder="Search..."
+    single-line
+    background-color="background-light"
+    dence
+    v-model="searchParam"
+    rounded
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  name: "app-search",
+  computed: {
+    searchParam: {
+      get() {
+        return this.$store.getters["appSearch/param"];
+      },
+      set(newValue) {
+        this.$store.commit("appSearch/setSearchParam", newValue);
+      },
+    },
+  },
+};
+</script>
