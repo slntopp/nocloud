09958d521f6fdc07c5d1ff518d0769ef93d96fa4
Mik Iwanowski <info@slnt-opp.xyz>
2022-10-31 14:58:03 +0100

    debug log

--- pkg/registry/accounts.go
+++ pkg/registry/accounts.go
@@ -204,6 +204,7 @@ func (s *AccountsServiceServer) Get(ctx context.Context, request *accountspb.Get
 		requested = requestor
 	}
 
+	log.Debug("Retrieving account", zap.String("uuid", requested))
 	acc, err := graph.GetWithAccess[graph.Account](ctx, s.db, driver.NewDocumentID(schema.ACCOUNTS_COL, request.GetUuid()))
 	if err != nil || acc.Access == nil {
 		log.Debug("Error getting account", zap.Any("error", err))
