28350df98d59d398ead5602e1496a3e68a5856e5
SazukinPavel <sazukinpavelal@gmail.com>
2022-10-31 13:50:35 +0300

    add langs

--- admin-ui/src/main.js
+++ admin-ui/src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import i18n from './i18n'
+import config from './config'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$config = config;
+
 new Vue({
+  i18n,
   router,
   store,
   vuetify,
