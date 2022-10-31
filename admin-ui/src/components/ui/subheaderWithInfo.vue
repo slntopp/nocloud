6dca3cfd6d5b629d57c95e617ab3ad53a48d25fc
SazukinPavel <sazukinpavelal@gmail.com>
2022-10-31 18:15:32 +0300

    add subheader with info component

---
+++ admin-ui/src/components/ui/subheaderWithInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-subheader>
+    <slot></slot>
+    <v-tooltip
+      open-on-click
+      :open-on-hover="false"
+      :open-on-focus="false"
+      v-if="infoText && isInfoExists"
+      bottom
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-icon class="ml-5" dark v-bind="attrs" v-on="on">
+          mdi-information-outline
+        </v-icon>
+      </template>
+      <p class="text subtitle-2 hint">{{ $t(infoText) }}</p>
+    </v-tooltip>
+  </v-subheader>
+</template>
+
+<script>
+export default {
+  name: "subheader-with-info",
+  props: {
+    infoText: { type: String },
+  },
+  computed: {
+    isInfoExists() {
+      return this.$t(this.infoText) !== this.infoText;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.hint {
+  max-width: 250px;
+  inline-size: 250px;
+  overflow-wrap: break-word;
+  user-select: text;
+}
+</style>
