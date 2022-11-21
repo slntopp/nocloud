9afb893ccb9e70ad5fa6a1cd8f762a650b0376fd
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-11-21 17:19:05 +0300

    Add nil check

--- pkg/registry/accounts.go
+++ pkg/registry/accounts.go
@@ -206,6 +206,12 @@ func (s *AccountsServiceServer) Get(ctx context.Context, request *accountspb.Get
 
 	log.Debug("Retrieving account", zap.String("uuid", requested))
 	acc, err := graph.GetWithAccess[graph.Account](ctx, s.db, driver.NewDocumentID(schema.ACCOUNTS_COL, requested))
+
+	if acc.Account == nil {
+		log.Debug("Error getting account", zap.Any("error", err))
+		return nil, status.Error(codes.NotFound, "Account not found")
+	}
+
 	if err != nil || acc.Access == nil {
 		log.Debug("Error getting account", zap.Any("error", err))
 		return nil, status.Error(codes.NotFound, "Account not found")
