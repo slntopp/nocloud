10c0216e55817a1cb7c48c1068723c340c278453
SazukinPavel <97695224+SazukinPavel@users.noreply.github.com>
2022-12-15 16:46:44 +0200

    Update api.js

--- admin-ui/src/api.js
+++ admin-ui/src/api.js
@@ -1,6 +1,6 @@
 import Api from "nocloudjsrest";
 import vuex from "@/store/index.js";
-const api = new Api("http://localhost/https://api.nocloud.ione-cloud.net", 8624);
+const api = new Api();
 
 api.axios.interceptors.response.use(
   function (response) {
