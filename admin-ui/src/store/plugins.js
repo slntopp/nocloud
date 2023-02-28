33a376231bfa58cd0b6264858ced2d5f1f227607
SazukinPavel <sazukinpavelal@gmail.com>
2023-02-28 16:35:03 +0300

    add plugins module

---
+++ admin-ui/src/store/plugins.js
@@ -0,0 +1,29 @@
+import api from "@/api.js";
+export default {
+  namespaced: true,
+  state: {
+    plugins: {},
+    loading: false,
+  },
+  getters: {
+    all(state) {
+      return state.plugins;
+    },
+    isLoading(state) {
+      return state.loading;
+    },
+  },
+  mutations: {
+    setPlugins(state, plugins) {
+      state.plugins = plugins;
+    },
+  },
+  actions: {
+    fetch({ commit }) {
+      api.settings.get(["plugins"]).then((res) => {
+        const key = res["plugins"];
+        if (key) commit("setPlugins", JSON.parse(key));
+      });
+    },
+  },
+};
