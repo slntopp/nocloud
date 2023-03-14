4ea397e1cff39ad494496dfb7cf8d3db4c5d03bd
oigen88 <sazukinpavelal@gmail.com>
2023-03-14 19:34:00 +0300

    add instance button

---
+++ admin-ui/src/components/ui/addInstance.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    small
+    id="button"
+    class="mx-2"
+    color="background"
+    @click="$emit('add')"
+    :disabled="disabled"
+  >
+    <v-icon dark> mdi-plus-circle-outline </v-icon>
+    add instance
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "addInstance",
+  emits: ["add"],
+  props: ["disabled"],
+};
+</script>
+
+<style scoped></style>
