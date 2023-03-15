c28abd78aea34bf0f481b1baff2be9977e4f180c
oigen88 <sazukinpavelal@gmail.com>
2023-03-15 19:18:37 +0300

    add create instance view in router

--- admin-ui/src/router/index.js
+++ admin-ui/src/router/index.js
@@ -170,6 +170,14 @@ const routes = [
     },
   },
   {
+    path: "/instances/create",
+    name: "Instance create",
+    component: () => import("../views/InstanceCreate.vue"),
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
     path: "/instances/:instanceId",
     name: "Instance",
     component: () => import("../views/InstancePage.vue"),
@@ -197,7 +205,7 @@ const routes = [
     path: "/login",
     name: "Login",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/login.vue"),
+        import(/* webpackChunkName: "about" */ "../views/login.vue"),
     meta: {
       requireUnlogin: true,
     },
