9e13a910233700c0cf29aa8625ce7a3110a2c0da
oigen88 <sazukinpavelal@gmail.com>
2023-03-21 15:14:30 +0300

    fix get state

--- admin-ui/src/views/Instances.vue
+++ admin-ui/src/views/Instances.vue
@@ -110,7 +110,6 @@
       v-model="selected"
       :column="column"
       :selected="selectedFilters"
-      :get-state="getState"
       :change-filters="changeFilters"
       @getHeaders="(value) => (headers = value)"
       @changeColumn="(value) => (column = value)"
@@ -145,6 +144,7 @@ import api from "@/api.js";
 import snackbar from "@/mixins/snackbar.js";
 import confirmDialog from "@/components/confirmDialog.vue";
 import instancesTable from "../components/instances_table.vue";
+import { getState } from "@/functions";
 
 export default {
   name: "instances-view",
@@ -237,7 +237,7 @@ export default {
               filter = this.getService(inst);
               break;
             case "Status":
-              filter = this.getState(inst).toLowerCase();
+              filter = getState(inst).toLowerCase();
               break;
             case "OS":
               filter = this.getOSName(inst.config.template_id, inst.sp);
@@ -258,28 +258,6 @@ export default {
           }
         }
       });
-    },
-    getState(item) {
-      if (!item.state) return "UNKNOWN";
-      const state =
-        item.billingPlan.type === "ione"
-          ? item.state.meta?.lcm_state_str
-          : item.state.state;
-
-      switch (item.state.meta.state) {
-        case 1:
-          return "PENDING";
-        case 5:
-          return "SUSPENDED";
-        case "BUILD":
-          return "BUILD";
-      }
-      switch (state) {
-        case "LCM_INIT":
-          return "POWEROFF";
-        default:
-          return state?.replaceAll("_", " ") ?? "";
-      }
     },
     getService({ service }) {
       return this.services.find(({ uuid }) => service === uuid)?.title ?? "";
