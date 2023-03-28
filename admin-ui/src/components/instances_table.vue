311a9c731015c1e48a22954b417ca533f9d71ec6
SazukinPavel <sazukinpavelal@gmail.com>
2023-03-28 18:57:39 +0300

    refactor instances table

--- admin-ui/src/components/instances_table.vue
+++ admin-ui/src/components/instances_table.vue
@@ -117,48 +117,20 @@
 
     <template v-slot:[`item.state.meta.networking`]="{ item }">
       <template v-if="!item.state?.meta.networking?.public">-</template>
-      <v-menu
-        bottom
-        open-on-hover
-        v-else
-        nudge-top="20"
-        nudge-left="15"
-        transition="slide-y-transition"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <span v-bind="attrs" v-on="on">
-            {{
-              item.state.meta.networking.public.find(
-                (ip) =>
-                  /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/gm.exec(
-                    ip
-                  ) || /\/32$/.exec(ip)
-              ) || item.state.meta.networking.public[0]
-            }}
-          </span>
-        </template>
-
-        <v-list dense>
-          <v-list-item
-            v-for="net of item.state.meta.networking.public"
-            :key="net"
-          >
-            <v-list-item-title>{{ net }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <instance-ip-menu v-else :item="item" />
     </template>
   </nocloud-table>
 </template>
 
 <script>
 import nocloudTable from "@/components/table.vue";
+import instanceIpMenu from "./ui/instanceIpMenu.vue";
 import api from "@/api";
 import { getState } from "@/functions";
 
 export default {
   name: "instances-table",
-  components: { nocloudTable },
+  components: { nocloudTable, instanceIpMenu },
   props: {
     value: { type: Array, required: true },
     selected: { type: Object, default: null },
