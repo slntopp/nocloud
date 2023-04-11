95dafdc86614ff75b8165a0f135901ea526886e7
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-11 12:37:25 +0300

    change position of table setting and reset filtres

--- admin-ui/src/components/table.vue
+++ admin-ui/src/components/table.vue
@@ -114,6 +114,43 @@
         </v-list>
       </v-menu>
     </template>
+      <template v-slot:[`header.data-table-select`]="{props,on}">
+          <div class="d-flex">
+              <v-simple-checkbox v-bind="props" v-on="on"></v-simple-checkbox>
+                  <v-dialog
+                          @click:outside="changeFiltres"
+                          max-width="60%"
+                          v-model="settingsDialog"
+                          width="500"
+                  >
+                      <template v-slot:activator="{ on, attrs }">
+                          <v-icon v-bind="attrs" v-on="on" size="23" class="mr-1"
+                          >mdi-cog-outline</v-icon
+                          >
+                      </template>
+                      <v-card
+                              color="background-light"
+                              style="overflow: hidden"
+                              max-width="100%"
+                      >
+                          <v-card-title>Table settings</v-card-title>
+                          <v-row class="pa-5">
+                              <v-col v-for="header in headers" :key="header.value" cols="4">
+                                  <v-checkbox
+                                          @click.stop
+                                          :label="header.text"
+                                          v-model="filter"
+                                          :value="header.value"
+                                  />
+                              </v-col>
+                          </v-row>
+                      </v-card>
+                  </v-dialog>
+                  <v-icon @click="resetFilter" size="23" class="mr-1s"
+                  >mdi-filter-remove</v-icon
+                  >
+          </div>
+      </template>
 
     <template v-if="footerError.length > 0" v-slot:footer>
       <v-toolbar class="mt-2" color="error" dark flat>
@@ -121,45 +158,6 @@
           {{ footerError }}
         </v-toolbar-title>
       </v-toolbar>
-    </template>
-    <template
-      v-slot:[`footer.page-text`]="{ pageStart, pageStop, itemsLength }"
-    >
-      <div class="d-flex align-center">
-        <v-dialog
-          @click:outside="changeFiltres"
-          max-width="60%"
-          v-model="settingsDialog"
-          width="500"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-icon v-bind="attrs" v-on="on" size="23" class="mr-3"
-              >mdi-cog-outline</v-icon
-            >
-          </template>
-          <v-card
-            color="background-light"
-            style="overflow: hidden"
-            max-width="100%"
-          >
-            <v-card-title>Table settings</v-card-title>
-            <v-row class="pa-5">
-              <v-col v-for="header in headers" :key="header.value" cols="4">
-                <v-checkbox
-                  @click.stop
-                  :label="header.text"
-                  v-model="filter"
-                  :value="header.value"
-                />
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-dialog>
-        <v-icon @click="resetFilter" size="23" class="mr-3"
-          >mdi-filter-remove</v-icon
-        >
-        <span>{{ `${pageStart}-${pageStop} of ${itemsLength}` }}</span>
-      </div>
     </template>
   </components>
 </template>
