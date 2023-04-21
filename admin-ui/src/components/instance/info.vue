49647dfa8ff82f20624d53d330d45960c9684d8a
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-21 11:46:24 +0300

    remove snapshots from info

--- admin-ui/src/components/instance/info.vue
+++ admin-ui/src/components/instance/info.vue
@@ -96,65 +96,6 @@
       @refresh="refreshInstance"
     />
 
-    <v-row class="flex-column mb-5" v-if="template.state">
-      <v-col>
-        <v-card-title class="mb-2 px-0">Snapshots:</v-card-title>
-        <v-menu
-          bottom
-          offset-y
-          transition="slide-y-transition"
-          v-model="isVisible"
-          :close-on-content-click="false"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn class="mr-2" v-bind="attrs" v-on="on"> Create </v-btn>
-          </template>
-          <v-card class="pa-4">
-            <v-row>
-              <v-col>
-                <v-text-field
-                  dense
-                  label="name"
-                  v-model="snapshotName"
-                  :rules="[(v) => !!v || 'Required!']"
-                />
-                <v-btn
-                  :loading="isLoading"
-                  @click="createSnapshot(template.uuid)"
-                >
-                  Send
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-menu>
-        <v-btn
-          class="mr-2"
-          :loading="isDeleteLoading"
-          @click="deleteSnapshot(template)"
-        >
-          Delete
-        </v-btn>
-        <v-btn :loading="isRevertLoading" @click="revertToSnapshot(template)">
-          Revert
-        </v-btn>
-      </v-col>
-      <v-col>
-        <nocloud-table
-          table-name="instanceInfo"
-          single-select
-          item-key="ts"
-          v-model="selected"
-          :items="Object.values(template.state?.meta?.snapshots || {})"
-          :headers="headers"
-        >
-          <template v-slot:[`item.ts`]="{ item }">
-            {{ date(item.ts) }}
-          </template>
-        </nocloud-table>
-      </v-col>
-    </v-row>
-
     <v-btn
       :to="{
         name: 'Instance edit',
@@ -201,7 +142,6 @@
 </template>
 
 <script>
-import api from "@/api.js";
 import snackbar from "@/mixins/snackbar.js";
 import nocloudTable from "@/components/table.vue";
 import instanceActions from "@/components/instance/controls.vue";
@@ -224,109 +164,9 @@ export default {
   data: () => ({
     copyed: null,
     templates: {},
-    selected: [],
-    headers: [
-      { text: "Name", value: "name" },
-      { text: "Time", value: "ts" },
-    ],
-    snapshotName: "Snapshot",
-
-    isVisible: false,
-    isLoading: false,
-    isDeleteLoading: false,
-    isRevertLoading: false,
-
     priceModelDialog: false,
   }),
   methods: {
-    createSnapshot(uuid) {
-      this.isLoading = true;
-
-      api.instances
-        .action({
-          uuid,
-          action: "snapcreate",
-          params: { snap_name: this.snapshotName },
-        })
-        .then(() => {
-          this.showSnackbarSuccess({
-            message: "Snapshot created successfully",
-          });
-        })
-        .catch((err) => {
-          this.showSnackbarError({
-            message: `Error: ${err?.response?.data?.message ?? "Unknown"}.`,
-          });
-        })
-        .finally(() => {
-          this.isLoading = false;
-          this.isVisible = false;
-        });
-    },
-    deleteSnapshot({ uuid, state }) {
-      const { snapshots } = state.meta;
-      const [id] = Object.entries(snapshots).find(
-        ([, el]) => el.ts === this.selected[0].ts
-      );
-
-      this.isDeleteLoading = true;
-      api.instances
-        .action({
-          uuid,
-          action: "snapdelete",
-          params: { snap_id: +id },
-        })
-        .then(() => {
-          this.showSnackbarSuccess({
-            message: "Snapshot deleted successfully",
-          });
-        })
-        .catch((err) => {
-          this.showSnackbarError({
-            message: `Error: ${err?.response?.data?.message ?? "Unknown"}.`,
-          });
-        })
-        .finally(() => {
-          this.isDeleteLoading = false;
-        });
-    },
-    revertToSnapshot({ uuid, state }) {
-      const { snapshots } = state.meta;
-      const [id] = Object.entries(snapshots).find(
-        ([, el]) => el.ts === this.selected[0].ts
-      );
-
-      this.isRevertLoading = true;
-      api.instances
-        .action({
-          uuid,
-          action: "snaprevert",
-          params: { snap_id: +id },
-        })
-        .then(() => {
-          this.showSnackbarSuccess({
-            message: "Snapshot reverted successfully",
-          });
-        })
-        .catch((err) => {
-          this.showSnackbarError({
-            message: `Error: ${err?.response?.data?.message ?? "Unknown"}.`,
-          });
-        })
-        .finally(() => {
-          this.isRevertLoading = false;
-        });
-    },
-    date(timestamp) {
-      const date = new Date(timestamp * 1000);
-      const time = date.toUTCString().split(" ")[4];
-
-      const day = date.getUTCDate();
-      const month = date.getUTCMonth() + 1;
-      const year = date.toUTCString().split(" ")[3];
-
-      return `${day}.${month}.${year} ${time}`;
-    },
     addToClipboard(text, index) {
       if (navigator?.clipboard) {
         navigator.clipboard
@@ -377,7 +217,10 @@ export default {
       return this.servicesProviders?.find((sp) => sp.uuid == this.template.sp);
     },
     filtredPlans() {
-      return this.plans.filter((p) => p.type === this.template.type || p.type.includes(this.template.type));
+      return this.plans.filter(
+        (p) =>
+          p.type === this.template.type || p.type.includes(this.template.type)
+      );
     },
     editPriceModelComponent() {
       const types = require.context(
