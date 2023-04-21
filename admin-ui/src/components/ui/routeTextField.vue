c96c93f4766e415ca75a1d0e7ba617a1942e8525
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-21 13:58:58 +0300

    add route text field component

---
+++ admin-ui/src/components/ui/routeTextField.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-text-field
+    @click:append="goTo(props.to)"
+    readonly
+    append-icon="mdi-login"
+    :label="props.label"
+    :value="props.value"
+  />
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import {useRouter} from "vue-router/composables";
+
+
+const props = defineProps({
+  to: {
+    type: Object,
+    required: true,
+  },
+  label: String,
+  value: String,
+});
+
+const router=useRouter()
+
+const goTo = ({name, params}) => {
+  router.push({ name, params });
+};
+</script>
+
+<style scoped></style>
