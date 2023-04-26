111e0e156388be147f96e123cb35d15152708b07
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-26 14:14:17 +0300

    add history tab to sp page

---
+++ admin-ui/src/components/ServicesProvider/history.vue
@@ -0,0 +1,15 @@
+<template>
+  <history-table hide-uuid table-name="service-history" :uuid="template.uuid" />
+</template>
+
+<script>
+import historyTable from "@/components/historyTable.vue";
+
+export default {
+  props: ["template"],
+  name: "sp-history",
+  components: { historyTable },
+};
+</script>
+
+<style scoped></style>
