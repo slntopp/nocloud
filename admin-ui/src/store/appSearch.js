15d9f1c156ec9a73cc250fe826d2665e223be21f
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-05-03 18:55:51 +0300

    add custom params in search

--- admin-ui/src/store/appSearch.js
+++ admin-ui/src/store/appSearch.js
@@ -2,34 +2,39 @@ export default {
   namespaced: true,
   state: {
     searchParam: "",
-    isAdvancedSearch: false,
     variants: {},
+    customParams: {},
   },
   mutations: {
     setSearchParam(state, newSearchParam) {
       state.searchParam = newSearchParam;
     },
-    setAdvancedSearch(state, val) {
-      state.isAdvancedSearch = val;
-    },
     setVariants(state, val) {
       state.variants = val;
     },
     resetSearchParams(state) {
-      state.isAdvancedSearch = false;
       state.searchParam = "";
-      state.variants = [];
+      state.variants = {};
+      state.customParams = {};
+    },
+    setCustomParam(state, { key, value }) {
+      // state.customParams[key] = value;
+      state.customParams={...state.customParams,[key]:value}
+    },
+    deleteCustomParam(state, key) {
+      delete state.customParams[key];
+      state.customParams={...state.customParams}
     },
   },
   getters: {
     param(state) {
       return state.searchParam;
     },
-    isAdvancedSearch(state) {
-      return state.isAdvancedSearch;
-    },
     variants(state) {
       return state.variants;
+    },
+    customParams(state) {
+      return state.customParams;
     },
   },
 };
