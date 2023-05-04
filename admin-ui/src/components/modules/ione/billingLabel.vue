1b411d41da44e5a73903984f1c5eaa497455af0d
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-05-04 14:36:58 +0300

    fix send vm action api

--- admin-ui/src/components/modules/ione/billingLabel.vue
+++ admin-ui/src/components/modules/ione/billingLabel.vue
@@ -11,7 +11,7 @@
         ><v-btn
           color="primary"
           :disabled="isRenewDisabled"
-          @click="sendVmAction('manual_renew', template.uuid)"
+          @click="sendVmAction('manual_renew', template)"
           >Renew</v-btn
         ></v-col
       >
