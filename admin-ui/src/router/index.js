f8421e39604a7f1ecf7ec66ba9b1c39f17f9a72b
SazukinPavel <97695224+SazukinPavel@users.noreply.github.com>
2023-05-17 15:10:33 +0300

    add mode history(delete #) (#832)
    * add mode history(delete #)
    
    * handle serve admin ui with history mode path
    
    * cleanup
    
    * update example
    
    ---------
    
    Co-authored-by: Mik Iwanowski <info@slnt-opp.xyz>

--- admin-ui/src/router/index.js
+++ admin-ui/src/router/index.js
@@ -301,8 +301,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  // mode: 'history',
-  base: `${process.env.BASE_URL}/`,
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes,
 });
 
