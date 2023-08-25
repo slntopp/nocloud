2a1a8068738dafd6711544f044ffbfc6843a8d47
PavelSazukin <sazukinpavelal@gmail.com>
2023-08-25 13:37:56 +0400

    add date picker component

---
+++ admin-ui/src/components/ui/datePicker.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-menu
+    :close-on-content-click="false"
+    transition="scale-transition"
+    min-width="auto"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        v-bind="attrs"
+        v-on="on"
+        prepend-inner-icon="mdi-calendar"
+        :value="value"
+        readonly
+        :label="label"
+      />
+    </template>
+    <v-date-picker
+      scrollable
+      :min="min"
+      :value="value"
+      @input="emits('input', $event)"
+    ></v-date-picker>
+  </v-menu>
+</template>
+
+<script setup>
+import { toRefs } from "vue";
+
+const props = defineProps(["value", "min", "label"]);
+const { value, min, label } = toRefs(props);
+
+const emits = defineEmits(["input"]);
+</script>
+
+<style scoped></style>
