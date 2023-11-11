7c3c919ce0734f51d7b2d6658d0335051e5de066
SazukinPavel <sazukinpavelal@gmail.com>
2023-11-11 14:27:33 +0400

    fix plans edit

--- admin-ui/src/views/PlansCreate.vue
+++ admin-ui/src/views/PlansCreate.vue
@@ -261,9 +261,10 @@ export default {
         value;
       }
 
-      const product = Object.values(this.plan.products)
-        .find((product) => product.id === id);
-      
+      const product = Object.values(this.plan.products).find(
+        (product) => product.id === id
+      );
+
       this.$set(product.meta, key, value);
       this.plan.meta = Object.assign({}, this.plan.meta);
     },
@@ -319,9 +320,12 @@ export default {
                 ? "Price model edited successfully"
                 : "Price model created successfully",
           });
-          setTimeout(() => {
-            this.$router.push({ name: "Plans" });
-          }, 100);
+          if (action !== "edit") {
+            setTimeout(() => {
+              this.$router.push({ name: "Plans" });
+            }, 100);
+          }
+          this.isDialogVisible=false
         })
         .catch((err) => {
           this.showSnackbarError({ message: err });
