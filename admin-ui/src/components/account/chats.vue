2079d977e08fa4ca89562e3e38612979dcc32e83
639852 <sasha.bobyliev@mail.ru>
2023-11-21 18:09:58 +0400

    - Added button for chats settings

---
+++ admin-ui/src/components/account/chats.vue
@@ -0,0 +1,15 @@
+<template>
+  <plugin-iframe
+    style="height: 100vh; width: 100%"
+    url="/cc.ui/"
+    :params="{ filterByAccount: $route.params.accountId }"
+  />
+</template>
+
+<script setup>
+import PluginIframe from "@/components/plugin/iframe.vue";
+</script>
+
+<script>
+export default { name: 'account-chats' }
+</script>
