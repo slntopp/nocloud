21e0828ff14cb83bbe299c0cac9c4d45eaa76518
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-12-07 17:49:44 +0400

    fix change plan kind

--- admin-ui/src/views/PlansCreate.vue
+++ admin-ui/src/views/PlansCreate.vue
@@ -15,9 +15,18 @@
               <v-subheader>Price model type</v-subheader>
             </v-col>
             <v-col cols="9">
-              <v-autocomplete label="Type" v-model="plan.type" :items="types" :rules="[rules.required]" />
-              <v-text-field label="Type name" v-if="plan.type === 'custom'" v-model="customTitle"
-                :rules="[rules.required]" />
+              <v-autocomplete
+                label="Type"
+                v-model="plan.type"
+                :items="types"
+                :rules="[rules.required]"
+              />
+              <v-text-field
+                label="Type name"
+                v-if="plan.type === 'custom'"
+                v-model="customTitle"
+                :rules="[rules.required]"
+              />
             </v-col>
           </v-row>
           <v-row align="center">
@@ -25,7 +34,11 @@
               <v-subheader>Price model title</v-subheader>
             </v-col>
             <v-col cols="9">
-              <v-text-field label="Title" v-model="plan.title" :rules="[rules.required]" />
+              <v-text-field
+                label="Title"
+                v-model="plan.title"
+                :rules="[rules.required]"
+              />
             </v-col>
           </v-row>
           <v-row align="center">
@@ -33,11 +46,21 @@
               <v-subheader>Price model kind</v-subheader>
             </v-col>
             <v-col cols="9">
-              <confirm-dialog @cancel="changePlan(true)" @confirm="changePlan">
-                <v-radio-group row mandatory v-model="selectedKind">
-                  <v-radio v-for="item in kinds" :key="item" :value="item" :label="item.toLowerCase()" />
-                </v-radio-group>
-              </confirm-dialog>
+              <v-radio-group row mandatory v-model="selectedKind">
+                <confirm-dialog
+                  @cancel="changePlan(true)"
+                  @confirm="changePlan"
+                >
+                  <div class="d-flex">
+                    <v-radio
+                      v-for="item in kinds"
+                      :key="item"
+                      :value="item"
+                      :label="item.toLowerCase()"
+                    />
+                  </div>
+                </confirm-dialog>
+              </v-radio-group>
             </v-col>
           </v-row>
 
@@ -46,7 +69,11 @@
               <v-subheader>Default tariff</v-subheader>
             </v-col>
             <v-col cols="9">
-              <v-autocomplete label="Tariff" v-model="plan.meta.product" :items="Object.keys(plan.products)" />
+              <v-autocomplete
+                label="Tariff"
+                v-model="plan.meta.product"
+                :items="Object.keys(plan.products)"
+              />
             </v-col>
           </v-row>
 
@@ -55,8 +82,13 @@
               <v-subheader>Linked price model</v-subheader>
             </v-col>
             <v-col cols="9">
-              <v-autocomplete clearable @change="plan.meta.linkedPlan = $event ?? undefined" label="Price model"
-                :value="plan.meta.linkedPlan" :items="filteredPlans" />
+              <v-autocomplete
+                clearable
+                @change="plan.meta.linkedPlan = $event ?? undefined"
+                label="Price model"
+                :value="plan.meta.linkedPlan"
+                :items="filteredPlans"
+              />
             </v-col>
           </v-row>
 
@@ -73,7 +105,10 @@
               <v-subheader>Auto start</v-subheader>
             </v-col>
             <v-col cols="9">
-              <v-switch style="width: fit-content" v-model="plan.meta.auto_start" />
+              <v-switch
+                style="width: fit-content"
+                v-model="plan.meta.auto_start"
+              />
             </v-col>
           </v-row>
         </v-col>
@@ -83,10 +118,17 @@
         </v-col>
 
         <v-col :cols="viewport > 2560 ? 6 : 12">
-          <component v-if="!productsHide" :is="template" :rules="rules" :type="plan.type" :resources="plan.resources"
-            :products="filteredProducts" @change:resource="(data) => changeConfig(data, 'resource')"
+          <component
+            v-if="!productsHide"
+            :is="template"
+            :rules="rules"
+            :type="plan.type"
+            :resources="plan.resources"
+            :products="filteredProducts"
+            @change:resource="(data) => changeConfig(data, 'resource')"
             @change:product="(data) => changeConfig(data, 'product')"
-            @change:meta="(data) => changeMetaConfig(data, 'meta')" />
+            @change:meta="(data) => changeMetaConfig(data, 'meta')"
+          />
         </v-col>
       </v-row>
 
@@ -95,30 +137,60 @@
           <v-btn class="mr-2" v-if="isEdit" @click="isDialogVisible = true">
             Save
           </v-btn>
-          <v-btn v-else class="mr-2" :loading="isLoading" @click="tryToSend('create')">
+          <v-btn
+            v-else
+            class="mr-2"
+            :loading="isLoading"
+            @click="tryToSend('create')"
+          >
             Create
           </v-btn>
-          <download-template-button :template="plan" :type="isJson ? 'JSON' : 'YAML'" :name="downloadedFileName" />
-          <v-switch class="d-inline-block mr-2" style="margin-top: 5px; padding-top: 0" v-model="isJson"
-            :label="!isJson ? 'YAML' : 'JSON'" />
-          <v-file-input class="file-input" v-if="!isEdit" :label="`upload ${isJson ? 'json' : 'yaml'} price model...`"
-            :accept="isJson ? '.json' : '.yaml'" @change="onJsonInputChange" />
+          <download-template-button
+            :template="plan"
+            :type="isJson ? 'JSON' : 'YAML'"
+            :name="downloadedFileName"
+          />
+          <v-switch
+            class="d-inline-block mr-2"
+            style="margin-top: 5px; padding-top: 0"
+            v-model="isJson"
+            :label="!isJson ? 'YAML' : 'JSON'"
+          />
+          <v-file-input
+            class="file-input"
+            v-if="!isEdit"
+            :label="`upload ${isJson ? 'json' : 'yaml'} price model...`"
+            :accept="isJson ? '.json' : '.yaml'"
+            @change="onJsonInputChange"
+          />
         </v-col>
       </v-row>
     </v-form>
 
     <v-dialog :max-width="600" v-model="isDialogVisible">
       <v-card color="background-light">
-        <v-card-title>Do you really want to change your current price model?</v-card-title>
-        <v-card-subtitle>You can also create a new price model based on the current
-          one.</v-card-subtitle>
+        <v-card-title
+          >Do you really want to change your current price model?</v-card-title
+        >
+        <v-card-subtitle
+          >You can also create a new price model based on the current
+          one.</v-card-subtitle
+        >
         <v-card-actions>
-          <v-btn class="mr-2" :loading="isLoading && savePlanAction === 'create'"
-            :disabled="isLoading && savePlanAction !== 'create'" @click="tryToSend('create')">
+          <v-btn
+            class="mr-2"
+            :loading="isLoading && savePlanAction === 'create'"
+            :disabled="isLoading && savePlanAction !== 'create'"
+            @click="tryToSend('create')"
+          >
             Create
           </v-btn>
-          <v-btn v-if="item" :loading="isLoading && savePlanAction === 'edit'"
-            :disabled="isLoading && savePlanAction !== 'edit'" @click="tryToSend('edit')">
+          <v-btn
+            v-if="item"
+            :loading="isLoading && savePlanAction === 'edit'"
+            :disabled="isLoading && savePlanAction !== 'edit'"
+            @click="tryToSend('edit')"
+          >
             Edit
           </v-btn>
         </v-card-actions>
@@ -161,7 +233,10 @@ export default {
       meta: {},
       fee: null,
     },
-    rules: { required: (v) => !!v || "This field is required!", price: (v) => (v !== '' && +v >= 0) || "Wrong price" },
+    rules: {
+      required: (v) => !!v || "This field is required!",
+      price: (v) => (v !== "" && +v >= 0) || "Wrong price",
+    },
 
     isDialogVisible: false,
     isVisible: true,
@@ -427,7 +502,7 @@ export default {
       return document.documentElement.clientWidth;
     },
     productsHide() {
-      const hidden = ["ovh", "goget", "acronis", "cpanel","keyweb"];
+      const hidden = ["ovh", "goget", "acronis", "cpanel", "keyweb"];
       return hidden.some((h) => this.plan.type?.includes(h));
     },
     filteredProducts() {
