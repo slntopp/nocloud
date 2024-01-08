4b8c2622b1e9c9a5b54be5a0a2740e3fb06b47f8
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2024-01-08 17:49:26 +0300

    Complete Addons service

---
+++ pkg/billing/addons.go
@@ -0,0 +1,128 @@
+package billing
+
+import (
+	"context"
+	"github.com/arangodb/go-driver"
+	"github.com/slntopp/nocloud-proto/access"
+	pb "github.com/slntopp/nocloud-proto/billing"
+	"github.com/slntopp/nocloud/pkg/graph"
+	"github.com/slntopp/nocloud/pkg/nocloud"
+	"github.com/slntopp/nocloud/pkg/nocloud/schema"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type AddonsServer struct {
+	pb.UnimplementedAddonsServiceServer
+
+	log *zap.Logger
+
+	db driver.Database
+
+	addons *graph.AddonsController
+	nss    graph.NamespacesController
+}
+
+func NewAddonsServer(logger *zap.Logger, db driver.Database) *AddonsServer {
+	log := logger.Named("AddonsServer")
+	return &AddonsServer{
+		log:    log,
+		db:     db,
+		addons: graph.NewAddonsController(log, db),
+		nss:    graph.NewNamespacesController(log.Named("PlansController"), db),
+	}
+}
+
+func (s *AddonsServer) Create(ctx context.Context, req *pb.Addon) (*pb.Addon, error) {
+	log := s.log.Named("Create")
+	requestor := ctx.Value(nocloud.NoCloudAccount).(string)
+
+	if !graph.HasAccess(ctx, s.db, requestor, schema.ROOT_NAMESPACE_KEY, access.Level_ADMIN) {
+		return nil, status.Error(codes.PermissionDenied, "Not enough Access rights to manage BillingPlans")
+	}
+
+	addon, err := s.addons.Create(ctx, req)
+	if err != nil {
+		log.Error("Failed to create document", zap.Error(err))
+		return nil, err
+	}
+
+	return addon, nil
+}
+
+func (s *AddonsServer) Update(ctx context.Context, req *pb.Addon) (*pb.Addon, error) {
+	log := s.log.Named("Update")
+	requestor := ctx.Value(nocloud.NoCloudAccount).(string)
+
+	if !graph.HasAccess(ctx, s.db, requestor, schema.ROOT_NAMESPACE_KEY, access.Level_ADMIN) {
+		return nil, status.Error(codes.PermissionDenied, "Not enough Access rights to manage BillingPlans")
+	}
+
+	addon, err := s.addons.Update(ctx, req)
+	if err != nil {
+		log.Error("Failed to update document", zap.Error(err))
+		return nil, err
+	}
+
+	return addon, nil
+}
+
+func (s *AddonsServer) Get(ctx context.Context, req *pb.Addon) (*pb.Addon, error) {
+	log := s.log.Named("Get")
+
+	requestor := ctx.Value(nocloud.NoCloudAccount).(string)
+
+	addon, err := s.addons.Get(ctx, req.GetUuid())
+	if err != nil {
+		log.Error("Failed to get document", zap.Error(err))
+		return nil, err
+	}
+
+	if !graph.HasAccess(ctx, s.db, requestor, schema.ROOT_NAMESPACE_KEY, access.Level_ADMIN) && !addon.GetPublic() {
+		return nil, status.Error(codes.PermissionDenied, "Not enough Access rights to manage BillingPlans")
+	}
+
+	return addon, nil
+}
+
+func (s *AddonsServer) List(ctx context.Context, req *pb.ListAddonsRequest) (*pb.ListAddonsResponse, error) {
+	log := s.log.Named("List")
+
+	requestor := ctx.Value(nocloud.NoCloudAccount).(string)
+
+	addons, err := s.addons.List(ctx, req.GetGroup())
+	if err != nil {
+		log.Error("Failed to get document", zap.Error(err))
+		return nil, err
+	}
+	if graph.HasAccess(ctx, s.db, requestor, schema.ROOT_NAMESPACE_KEY, access.Level_ADMIN) {
+		return &pb.ListAddonsResponse{Addons: addons}, nil
+	}
+
+	var filteredAddons []*pb.Addon
+
+	for _, val := range filteredAddons {
+		if val.GetPublic() {
+			filteredAddons = append(filteredAddons, val)
+		}
+	}
+	return &pb.ListAddonsResponse{Addons: filteredAddons}, nil
+}
+
+func (s *AddonsServer) Delete(ctx context.Context, req *pb.Addon) (*pb.Addon, error) {
+	log := s.log.Named("Create")
+	requestor := ctx.Value(nocloud.NoCloudAccount).(string)
+
+	if !graph.HasAccess(ctx, s.db, requestor, schema.ROOT_NAMESPACE_KEY, access.Level_ADMIN) {
+		return nil, status.Error(codes.PermissionDenied, "Not enough Access rights to manage BillingPlans")
+	}
+
+	err := s.addons.Delete(ctx, req)
+	if err != nil {
+		log.Error("Failed to create document", zap.Error(err))
+		return nil, err
+	}
+
+	return nil, nil
+}
---
+++ pkg/graph/addons.go
@@ -0,0 +1,106 @@
+package graph
+
+import (
+	"context"
+	"fmt"
+	"github.com/arangodb/go-driver"
+	pb "github.com/slntopp/nocloud-proto/billing"
+	"github.com/slntopp/nocloud/pkg/nocloud/schema"
+	"go.uber.org/zap"
+)
+
+type AddonsController struct {
+	log *zap.Logger
+	col driver.Collection
+}
+
+func NewAddonsController(logger *zap.Logger, db driver.Database) *AddonsController {
+	ctx := context.TODO()
+	log := logger.Named("AddonsController")
+	addons := GetEnsureCollection(log, ctx, db, schema.ADDONS_COL)
+	return &AddonsController{
+		log: log, col: addons,
+	}
+}
+
+func (c *AddonsController) Create(ctx context.Context, addon *pb.Addon) (*pb.Addon, error) {
+	log := c.log.Named("Create")
+
+	document, err := c.col.CreateDocument(ctx, addon)
+	if err != nil {
+		log.Error("Failed to create document", zap.Error(err))
+		return nil, err
+	}
+
+	addon.Uuid = document.Key
+	return addon, nil
+}
+
+func (c *AddonsController) Update(ctx context.Context, addon *pb.Addon) (*pb.Addon, error) {
+	log := c.log.Named("Update")
+
+	_, err := c.col.UpdateDocument(ctx, addon.GetUuid(), addon)
+	if err != nil {
+		log.Error("Failed to update document", zap.Error(err))
+		return nil, err
+	}
+
+	return addon, nil
+}
+
+func (c *AddonsController) Delete(ctx context.Context, addon *pb.Addon) error {
+	log := c.log.Named("Update")
+
+	_, err := c.col.RemoveDocument(ctx, addon.GetUuid())
+	if err != nil {
+		log.Error("Failed to update document", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
+
+func (c *AddonsController) Get(ctx context.Context, uuid string) (*pb.Addon, error) {
+	log := c.log.Named("Get")
+
+	var addon pb.Addon
+
+	_, err := c.col.ReadDocument(ctx, uuid, &addon)
+	if err != nil {
+		log.Error("Failed to get document", zap.Error(err))
+		return nil, err
+	}
+
+	return &addon, nil
+}
+
+func (c *AddonsController) List(ctx context.Context, group string) ([]*pb.Addon, error) {
+	log := c.log.Named("Get")
+
+	query := "LET adds = (FOR a in @@addons %s RETURN a)"
+	vars := map[string]any{
+		"@addons": schema.ADDONS_COL,
+	}
+
+	if group != "" {
+		query = fmt.Sprintf(query, "FILTER a.group == @group")
+		vars["group"] = group
+	} else {
+		query = fmt.Sprintf(query, "")
+	}
+
+	cur, err := c.col.Database().Query(ctx, query, vars)
+	if err != nil {
+		log.Error("Failed to get documents", zap.Error(err))
+		return nil, err
+	}
+
+	var addons []*pb.Addon
+	_, err = cur.ReadDocument(ctx, &addons)
+	if err != nil {
+		log.Error("Failed to read documents", zap.Error(err))
+		return nil, err
+	}
+
+	return addons, nil
+}
