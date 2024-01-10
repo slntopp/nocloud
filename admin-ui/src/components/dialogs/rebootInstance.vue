3db4d330dd545d7d69091299e2d5eecf4cd36156
Pavel Sazukin <sazukinpavelal@gmail.com>
2024-01-10 14:32:49 +0400

    add icons to all instance actions

--- admin-ui/src/components/dialogs/rebootInstance.vue
+++ admin-ui/src/components/dialogs/rebootInstance.vue
@@ -1,28 +1,33 @@
 <template>
-  <v-dialog v-model="isModalOpen" max-width="500">
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn
-        :disabled="disabled"
-        :loading="loading"
-        class="ma-1"
-        v-bind="attrs"
-        v-on="on"
-      >
-        reboot
-      </v-btn>
-    </template>
-    <v-card color="background-light">
-      <v-card-title>Select reboot type</v-card-title>
-      <v-card-actions class="d-flex justify-end">
-        <v-btn class="mr-2" @click="reboot('hard')"> Hard </v-btn>
-        <v-btn class="mr-2" @click="reboot('soft')"> Soft </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <instance-control-btn hint="reboot">
+    <v-dialog v-model="isModalOpen" max-width="500">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+            :disabled="disabled"
+            :loading="loading"
+            class="ma-1"
+            v-bind="attrs"
+            v-on="on"
+        >
+          <v-icon>
+            mdi-restart
+          </v-icon>
+        </v-btn>
+      </template>
+      <v-card color="background-light">
+        <v-card-title>Select reboot type</v-card-title>
+        <v-card-actions class="d-flex justify-end">
+          <v-btn class="mr-2" @click="reboot('hard')"> Hard </v-btn>
+          <v-btn class="mr-2" @click="reboot('soft')"> Soft </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </instance-control-btn>
 </template>
 
 <script setup>
 import { toRefs, ref } from "vue";
+import InstanceControlBtn from "@/components/ui/instanceControlBtn.vue";
 
 const props = defineProps(["disabled", "loading", "template"]);
 const emit = defineEmits(["click"]);
