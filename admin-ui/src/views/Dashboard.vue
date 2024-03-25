2f46edf315e7596b5df484fe30185a6156c249c5
Pavel Sazukin <sazukinpavelal@gmail.com>
2024-03-25 19:00:06 +0400

    add widgets save/load data

--- admin-ui/src/views/Dashboard.vue
+++ admin-ui/src/views/Dashboard.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="widgets align-start gg-15px pa-4 d-flex flex-wrap">
     <component
-      v-for="widget of widgets"
-      :key="widget"
-      :is="widget"
+      v-for="{ key, component } of widgets"
+      :key="key"
+      :is="component"
       style="width: 33%"
+      :data="widgetsData[key] || {}"
+      @update="updateWidgetData(key, $event)"
+      @update:key="updateWidgetDataKey(key, $event)"
     />
   </div>
 </template>
@@ -16,25 +19,63 @@ import InstancesWidget from "@/components/widgets/instances";
 import InstancesTypesWidget from "@/components/widgets/instances-types.vue";
 import InstancesPricesWidget from "@/components/widgets/instances-prices.vue";
 import ChatsWidget from "@/components/widgets/chats.vue";
-import { onMounted } from "vue";
+import { onBeforeUnmount, onMounted, ref } from "vue";
 import { useStore } from "@/store/";
 
 const store = useStore();
 
 const widgets = [
-  ChatsWidget,
-  AccountsWidget,
-  InstancesWidget,
-  TransactionsWidget,
-  InstancesTypesWidget,
-  InstancesPricesWidget,
+  { component: ChatsWidget, key: "chats" },
+  { component: AccountsWidget, key: "accounts" },
+  { component: InstancesWidget, key: "instances" },
+  { component: TransactionsWidget, key: "transactions" },
+  { component: InstancesTypesWidget, key: "instances-types" },
+  { component: InstancesPricesWidget, key: "instances-prices" },
 ];
 
+const widgetsData = ref({
+  chats: {},
+  accounts: {},
+  instances: {},
+  transactions: {},
+  "instances-types": {},
+  "instances-prices": {},
+});
+
 onMounted(() => {
+  loadWidgetsData();
+
   store.commit("reloadBtn/setCallback", {
     event: () => this.$router.go(),
   });
+  window.addEventListener("beforeunload", saveWidgetsData);
 });
+
+onBeforeUnmount(() => {
+  saveWidgetsData();
+  window.removeEventListener("beforeunload", saveWidgetsData);
+});
+
+const updateWidgetData = (key, data) => {
+  widgetsData.value[key] = data;
+};
+
+const updateWidgetDataKey = (key, { key: subkey, value }) => {
+  widgetsData.value[key][subkey] = value;
+};
+
+const saveWidgetsData = () => {
+  localStorage.setItem("nocloud-widgets", JSON.stringify(widgetsData.value));
+};
+
+const loadWidgetsData = () => {
+  try {
+    const data = JSON.parse(localStorage.getItem("nocloud-widgets"));
+    widgetsData.value = { ...widgetsData.value, ...data };
+  } catch (e) {
+    console.log(e);
+  }
+};
 </script>
 
 <script>
