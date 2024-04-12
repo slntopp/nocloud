01ac48749c9f8c02909be87afce478c89a6fc831
coddmeistr <euseew.maxim2015@yandex.ru>
2024-04-12 16:56:37 +0300

    fix currency entries: registry

--- pkg/registry/accounts.go
+++ pkg/registry/accounts.go
@@ -431,7 +431,7 @@ func (s *AccountsServiceServer) Create(ctx context.Context, request *accountspb.
 
 	creationAccount := accountspb.Account{
 		Title:    request.Title,
-		Currency: &request.Currency,
+		Currency: request.Currency,
 		Data:     request.GetData(),
 	}
 
@@ -529,7 +529,7 @@ func (s *AccountsServiceServer) SignUp(ctx context.Context, request *accountspb.
 
 	creationAccount := accountspb.Account{
 		Title:    request.Title,
-		Currency: &request.Currency,
+		Currency: request.Currency,
 		Data:     request.GetData(),
 		Status:   accStatus,
 	}
