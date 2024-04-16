758e5ea972f3c9cc1cfff7e6a395d371927e4591
Pavel Sazukin <sazukinpavelal@gmail.com>
2024-04-16 14:03:43 +0400

    add ione instance addons

--- admin-ui/src/components/modules/ione/billingLabel.vue
+++ admin-ui/src/components/modules/ione/billingLabel.vue
@@ -2,7 +2,7 @@
   <billing-label
     :account="account"
     :template="template"
-    :addons-price="addonsPrice"
+    :addons-price="addonsPrices"
     :tariff-price="tariffPrice"
     :due-date="dueDate"
     :renew-disabled="isRenewDisabled"
@@ -15,43 +15,45 @@ import { computed, ref, toRefs } from "vue";
 import { formatSecondsToDate } from "@/functions";
 import billingLabel from "@/components/ui/billingLabel.vue";
 
-const props = defineProps(["template", "account"]);
+const props = defineProps(["template", "account", "addons"]);
 const emit = defineEmits(["update"]);
 
-const { template, account } = toRefs(props);
+const { template, account, addons } = toRefs(props);
 
 const tariffPrice = ref(
   template.value.billingPlan.products[template.value.product]?.price ?? 0
 );
-const addonsPrice = ref(
-  template.value.billingPlan.resources.reduce((prev, curr) => {
+const addonsPrices = computed(() => {
+  const prices = {};
+  console.log(addons);
+  template.value.billingPlan.resources.forEach((curr) => {
     if (
       curr.key === `drive_${template.value.resources.drive_type.toLowerCase()}`
     ) {
       const key = "drive";
 
-      return {
-        ...prev,
-        [key]: (curr.price * template.value.resources.drive_size) / 1024,
-      };
+      return (prices[key] =
+        (curr.price * template.value.resources.drive_size) / 1024);
     } else if (curr.key === "ram") {
       const key = "ram";
 
-      return {
-        ...prev,
-        [key]: (curr.price * template.value.resources.ram) / 1024,
-      };
+      return (prices[key] = (curr.price * template.value.resources.ram) / 1024);
     } else if (template.value.resources[curr.key]) {
       const key = curr.key.replace("_", " ");
 
-      return {
-        ...prev,
-        [key]: curr.price * template.value.resources[curr.key],
-      };
+      return (prices[key] = curr.price * template.value.resources[curr.key]);
     }
-    return prev;
-  }, {})
-);
+  });
+
+  addons.value.forEach(
+    (a) =>
+      (prices[a.uuid] =
+        a.periods[
+          template.value.billingPlan.products[template.value.product]?.period
+        ])
+  );
+  return prices;
+});
 
 const dueDate = computed(() => {
   return formatSecondsToDate(+template.value?.data?.next_payment_date);
