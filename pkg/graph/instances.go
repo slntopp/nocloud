3c71a8b1f397baa08e759e354dd517ddbed6a176
coddmeistr <euseew.maxim2015@yandex.ru>
2024-04-23 11:44:14 +0300

    invoice creation on instance creation

--- pkg/graph/instances.go
+++ pkg/graph/instances.go
@@ -57,7 +57,9 @@ type InstancesController struct {
 
 	ig2inst driver.Collection
 
-	inv *InvoicesController
+	inv InvoicesController
+	acc AccountsController
+	cur CurrencyController
 }
 
 func NewInstancesController(log *zap.Logger, db driver.Database) *InstancesController {
@@ -67,7 +69,8 @@ func NewInstancesController(log *zap.Logger, db driver.Database) *InstancesContr
 	col := GetEnsureCollection(log, ctx, db, schema.INSTANCES_COL)
 	ig2inst := GraphGetEdgeEnsure(log, ctx, graph, schema.IG2INST, schema.INSTANCES_GROUPS_COL, schema.INSTANCES_COL)
 
-	return &InstancesController{log: log.Named("InstancesController"), col: col, graph: graph, db: db, ig2inst: ig2inst}
+	return &InstancesController{log: log.Named("InstancesController"), col: col, graph: graph, db: db, ig2inst: ig2inst,
+		inv: NewInvoicesController(log, db), acc: NewAccountsController(log, db), cur: NewCurrencyController(log, db)}
 }
 
 func (ctrl *InstancesController) Create(ctx context.Context, group driver.DocumentID, sp string, i *pb.Instance) error {
@@ -123,12 +126,38 @@ func (ctrl *InstancesController) Create(ctx context.Context, group driver.Docume
 		return err
 	}
 
+	// =================================
 	// Create invoice for this instance
+	var errInvoiceCreation = "instance was created. Failed to create invoice for this instance: %v"
+	accId, ok := ctx.Value(nocloud.NoCloudAccount).(string)
+	if !ok {
+		log.Error("Failed to get account id")
+		return fmt.Errorf(errInvoiceCreation, "no related account found in context")
+	}
+	acc, err := ctrl.acc.Get(ctx, accId)
+	if err != nil {
+		log.Error("Failed to get account", zap.Error(err))
+		return fmt.Errorf(errInvoiceCreation, err)
+	}
+
+	var accCurrency = &bpb.Currency{Id: schema.DEFAULT_CURRENCY_ID, Title: schema.DEFAULT_CURRENCY_NAME}
+	if acc.Currency != nil {
+		accCurrency = acc.Currency
+	}
+
+	rate, err := ctrl.cur.GetExchangeRateDirect(ctx,
+		&bpb.Currency{Id: schema.DEFAULT_CURRENCY_ID},
+		accCurrency)
+	if err != nil {
+		log.Error("Failed to get exchange rate", zap.Error(err))
+		return fmt.Errorf(errInvoiceCreation, err)
+	}
+
 	// Exec = Now + 24hours
 	// TODO: review cost
 	var cost float64
 	for _, p := range i.GetBillingPlan().GetProducts() {
-		cost += p.Price
+		cost += p.Price * rate
 	}
 	_, err = ctrl.inv.Create(ctx, &bpb.Invoice{
 		Status: bpb.BillingStatus_UNPAID,
@@ -139,14 +168,16 @@ func (ctrl *InstancesController) Create(ctx context.Context, group driver.Docume
 				Instance: i.GetUuid(),
 			},
 		},
-		Total:   float64(cost),
-		Type:    bpb.ActionType_INSTANCE_CREATION,
-		Created: time.Now().Unix(),
-		Exec:    time.Now().Add(24 * time.Hour).Unix(),
+		Total:    cost,
+		Type:     bpb.ActionType_INSTANCE_CREATION,
+		Created:  time.Now().Unix(),
+		Exec:     time.Now().Add(24 * time.Hour).Unix(),
+		Account:  acc.GetUuid(),
+		Currency: accCurrency,
 	})
 	if err != nil {
 		log.Error("Failed to create Invoice", zap.Error(err))
-		return fmt.Errorf("instance was created. Failed to create invoice for this instaqnce: %v", err)
+		return fmt.Errorf(errInvoiceCreation, err)
 	}
 
 	return nil
