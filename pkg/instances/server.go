51660251fb726e84eecc182524e0d3698dafbb24
coddmeistr <euseew.maxim2015@yandex.ru>
2024-05-22 20:58:21 +0300

    estimate and period in list

--- pkg/instances/server.go
+++ pkg/instances/server.go
@@ -660,5 +660,16 @@ func (s *InstancesServer) List(ctx context.Context, req *pb.ListInstancesRequest
 		return nil, err
 	}
 
+	// Calculate estimate and period values if not presented
+	for _, value := range result.Pool {
+		inst := value.Instance
+		if inst.GetEstimate() == 0 {
+			inst.Estimate, _ = s.ctrl.CalculateInstanceEstimatePeriodicPrice(inst)
+		}
+		if inst.GetPeriod() == 0 {
+			inst.Period, _ = s.ctrl.GetInstancePeriod(inst)
+		}
+	}
+
 	return &result, nil
 }
