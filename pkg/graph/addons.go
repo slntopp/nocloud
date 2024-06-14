001cc04f90b74e0388332dda0a434afe89d2c336
coddmeistr <euseew.maxim2015@yandex.ru>
2024-06-14 14:03:37 +0300

    addons: fix system filter

--- pkg/graph/addons.go
+++ pkg/graph/addons.go
@@ -120,7 +120,8 @@ func (c *AddonsController) List(ctx context.Context, req *pb.ListAddonsRequest) 
 			if key == "title" {
 				query += fmt.Sprintf(` FILTER a.title LIKE "%s"`, "%"+value.GetStringValue()+"%")
 			} else if key == "system" {
-				query += fmt.Sprintf(` FILTER a.system == %t`, value.GetBoolValue())
+				// TODO: make it easier to read
+				query += fmt.Sprintf(` FILTER (%t && a.system == true) || (!%t && (!a.system || a.system == false))`, value.GetBoolValue(), value.GetBoolValue())
 			} else if key == "search_param" {
 				query += fmt.Sprintf(` FILTER LOWER(a.title) LIKE LOWER("%s") || LOWER(a.group) LIKE LOWER("%s") || a._key LIKE "%s"`, "%"+value.GetStringValue()+"%", "%"+value.GetStringValue()+"%", "%"+value.GetStringValue()+"%")
 			} else {
