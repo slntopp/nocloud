7e8b9a8a07422202403b77ce0037cd45ed48f27d
SazukinPavel <sazukinpavelal@gmail.com>
2024-07-17 16:09:02 +0400

    optimize plan instances

--- admin-ui/src/components/plan/instances.vue
+++ admin-ui/src/components/plan/instances.vue
@@ -3,12 +3,16 @@
     <div class="d-flex justify-space-between">
       <span> Instances </span>
     </div>
-    <instances-table :value="selected" :items="instances" />
+    <instances-table
+      :show-select="false"
+      :custom-filter="{ billing_plan: [template.uuid] }"
+      table-name="plan-instances-table"
+    />
   </v-card>
 </template>
 
 <script>
-import InstancesTable from "@/components/instances_table.vue";
+import InstancesTable from "@/components/instancesTable.vue";
 
 export default {
   name: "instances-tab",
@@ -17,23 +21,7 @@ export default {
 </script>
 
 <script setup>
-import { useStore } from "@/store";
-import { computed, onMounted, defineProps, ref } from "vue";
-const props = defineProps(["template"]);
+import { defineProps } from "vue";
 
-const store = useStore();
-
-const selected = ref([]);
-// const isUpdateAllLoading = ref(false);
-
-const instances = computed(() =>
-  store.getters["services/getInstances"].filter(
-    (i) => i.billingPlan.uuid === props.template?.uuid
-  )
-);
-
-onMounted(() => {
-  store.dispatch("services/fetch", { showDeleted: true });
-  store.dispatch("namespaces/fetch");
-});
+defineProps(["template"]);
 </script>
