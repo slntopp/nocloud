61c533528f3e89ec0abdb9ad018c270ff5d4deb0
coddmeistr <euseew.maxim2015@yandex.ru>
2024-07-22 05:51:55 +0300

    invoices: change graph struct

--- pkg/graph/invoices.go
+++ pkg/graph/invoices.go
@@ -3,12 +3,22 @@ package graph
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/arangodb/go-driver"
 	pb "github.com/slntopp/nocloud-proto/billing"
 	"github.com/slntopp/nocloud/pkg/graph/migrations"
 	"github.com/slntopp/nocloud/pkg/nocloud/schema"
 	"go.uber.org/zap"
+	"strings"
+	"time"
 )
+
+type Invoice struct {
+	*pb.Invoice
+	NumericNumber  int    `json:"numeric_number"`
+	NumberTemplate string `json:"number_template"`
+	driver.DocumentMeta
+}
 
 type InvoicesController struct {
 	col driver.Collection // Billing Plans collection
@@ -28,7 +38,18 @@ func NewInvoicesController(logger *zap.Logger, db driver.Database) InvoicesContr
 	}
 }
 
-func (ctrl *InvoicesController) Create(ctx context.Context, tx *pb.Invoice) (*pb.Invoice, error) {
+func (ctrl *InvoicesController) ParseNumberIntoTemplate(template string, number int, date time.Time) string {
+	year := date.Year()
+	month := int(date.Month())
+	day := date.Day()
+	template = strings.Replace(template, "{YEAR}", fmt.Sprintf("%d", year), -1)
+	template = strings.Replace(template, "{MONTH}", fmt.Sprintf("%d", month), -1)
+	template = strings.Replace(template, "{DAY}", fmt.Sprintf("%d", day), -1)
+	template = strings.Replace(template, "{NUMBER}", fmt.Sprintf("%d", number), -1)
+	return template
+}
+
+func (ctrl *InvoicesController) Create(ctx context.Context, tx *Invoice) (*Invoice, error) {
 	if tx.GetAccount() == "" {
 		return nil, errors.New("account is required")
 	}
@@ -44,21 +65,21 @@ func (ctrl *InvoicesController) Create(ctx context.Context, tx *pb.Invoice) (*pb
 	return tx, nil
 }
 
-func (ctrl *InvoicesController) Get(ctx context.Context, uuid string) (*pb.Invoice, error) {
-	var tx pb.Invoice
+func (ctrl *InvoicesController) Get(ctx context.Context, uuid string) (*Invoice, error) {
+	var tx Invoice
 	meta, err := ctrl.col.ReadDocument(ctx, uuid, &tx)
 	if err != nil {
-		ctrl.log.Error("Failed to read transaction", zap.Error(err))
+		ctrl.log.Error("Failed to read invoice", zap.Error(err))
 		return nil, err
 	}
 	tx.Uuid = meta.Key
 	return &tx, nil
 }
 
-func (ctrl *InvoicesController) Update(ctx context.Context, tx *pb.Invoice) (*pb.Invoice, error) {
+func (ctrl *InvoicesController) Update(ctx context.Context, tx *Invoice) (*Invoice, error) {
 	_, err := ctrl.col.UpdateDocument(ctx, tx.GetUuid(), tx)
 	if err != nil {
-		ctrl.log.Error("Failed to update transaction", zap.Error(err))
+		ctrl.log.Error("Failed to update invoice", zap.Error(err))
 		return nil, err
 	}
 	return tx, nil
