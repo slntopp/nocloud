ab6d775f269a1c8421d32a1810cc165460b8eee3
SazukinPavel <sazukinpavelal@gmail.com>
2024-08-08 14:13:24 +0400

    add empty resources ui

---
+++ admin-ui/src/components/modules/empty/additionalInstanceInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-row>
+      <v-col v-for="key in Object.keys(template.resources)" :key="key">
+        <v-text-field :label="key" :value="template.resources[key]" readonly />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script setup>
+import { toRefs, defineProps } from "vue";
+
+const props = defineProps(["template", "sp"]);
+
+const { template } = toRefs(props);
+</script>
+
+<style scoped></style>
