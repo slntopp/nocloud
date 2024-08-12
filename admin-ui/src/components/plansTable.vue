3ea59209812c83f37a5c5e8399d130935faf34b5
SazukinPavel <sazukinpavelal@gmail.com>
2024-08-12 21:21:41 +0400

    add plans table

---
+++ admin-ui/src/components/plansTable.vue
@@ -0,0 +1,419 @@
+<template>
+  <nocloud-table
+    :table-name="tableName"
+    class="mt-4"
+    :items="plans"
+    :headers="headers"
+    :value="value"
+    @input="emit('input', $event)"
+    :loading="isLoading"
+    :footer-error="fetchError"
+    @update:options="setOptions"
+    :server-items-length="total"
+    :server-side-page="page"
+  >
+    <template v-slot:[`item.title`]="{ item }">
+      <router-link :to="{ name: 'Plan', params: { planId: item.uuid } }">
+        {{ getShortName(item.title, 45) }}
+      </router-link>
+    </template>
+    <template v-slot:[`item.meta.auto_start`]="{ item }">
+      <v-skeleton-loader v-if="updatedPlanUuid === item.uuid" type="text" />
+      <v-switch
+        v-else
+        dense
+        hide-details
+        :readonly="!!updatedPlanUuid"
+        :input-value="item.meta?.auto_start"
+        :disabled="isDeleted(item)"
+        @change="
+          updatePlan(item, {
+            key: 'meta',
+            value: { ...(item.meta || {}), auto_start: $event },
+          })
+        "
+      />
+    </template>
+    <template v-slot:[`item.public`]="{ item }">
+      <v-skeleton-loader v-if="updatedPlanUuid === item.uuid" type="text" />
+      <v-switch
+        v-else
+        dense
+        hide-details
+        :readonly="!!updatedPlanUuid"
+        :input-value="item.public"
+        :disabled="isDeleted(item)"
+        @change="
+          updatePlan(item, {
+            key: 'public',
+            value: $event,
+          })
+        "
+      />
+    </template>
+    <template v-slot:[`item.kind`]="{ value }">
+      {{ value?.toLowerCase && value?.toLowerCase() }}
+    </template>
+    <template v-slot:[`item.instanceCount`]="{ item }">
+      <v-progress-circular
+        v-if="isInstanceCountLoading"
+        size="20"
+        indeterminate
+      />
+      <template v-else>
+        {{ instanceCountMap[item.uuid] }}
+      </template>
+    </template>
+    <template v-slot:[`item.status`]="{ item }">
+      <v-chip small :color="getStatus(item).color">
+        {{ getStatus(item).title }}
+      </v-chip>
+    </template>
+    <template v-slot:footer.prepend>
+      <div class="d-flex align-center mt-2">
+        <v-select
+          style="max-width: 80px"
+          :items="fileTypes"
+          label="File type"
+          v-model="selectedFileType"
+          class="d-inline-block mx-1"
+        />
+        <download-template-button
+          @click:xlsx="downloadXlsx"
+          class="mx-1"
+          small
+          title="Download"
+          :disabled="!value.length || isPlansUploadLoading"
+          name="selected_copy"
+          :type="selectedFileType"
+          :template="value"
+        />
+        <v-file-input
+          class="file-input mx-1 mt-4"
+          :label="`upload ${selectedFileType} price models...`"
+          :accept="`.${selectedFileType}`"
+          @change="onJsonInputChange"
+        />
+        <confirm-dialog
+          @confirm="uploadPlans"
+          :disabled="!uploadedPlans.length"
+          :text="uploadedPlansText"
+        >
+          <v-btn
+            :disabled="!uploadedPlans.length"
+            :loading="isPlansUploadLoading"
+            small
+            >Upload</v-btn
+          >
+        </confirm-dialog>
+      </div>
+    </template>
+  </nocloud-table>
+</template>
+
+<script setup>
+import { computed, onMounted, ref, toRefs, watch } from "vue";
+import { useStore } from "@/store";
+import nocloudTable from "@/components/table.vue";
+import confirmDialog from "@/components/confirmDialog.vue";
+import DownloadTemplateButton from "@/components/ui/downloadTemplateButton.vue";
+import {
+  downloadPlanXlsx,
+  readJSONFile,
+  readYAMLFile,
+  getShortName,
+  debounce,
+} from "@/functions";
+
+const statusMap = {
+  DEL: { title: "DELETED", color: "blue-grey darken-2" },
+  UNSPECIFIED: { title: "ACTIVE", color: "success" },
+};
+
+const props = defineProps({
+  value: {},
+  tableName: { type: String, default: "plans-table" },
+  showSelect: { type: Boolean, default: false },
+  customParams: { type: Object, default: () => ({}) },
+  refetch: { type: Boolean, default: false },
+});
+const { value, refetch, customParams } = toRefs(props);
+
+const emit = defineEmits(["input", "update:options"]);
+
+const store = useStore();
+
+const headers = ref([
+  { text: "Title ", value: "title" },
+  { text: "UUID ", value: "uuid" },
+  { text: "Kind ", value: "kind" },
+  { text: "Type ", value: "type" },
+  { text: "Status ", value: "status" },
+  { text: "Public ", value: "public" },
+  { text: "Auto start ", value: "meta.auto_start" },
+  { text: "Linked instances count ", value: "instanceCount" },
+]);
+
+const fetchError = ref("");
+const options = ref({});
+const page = ref(1);
+
+const unique = ref({});
+
+const isInstanceCountLoading = ref(true);
+const instanceCountMap = ref({});
+
+const updatedPlanUuid = ref("");
+
+const isPlansUploadLoading = ref(false);
+const uploadedPlans = ref([]);
+const selectedFileType = ref("JSON");
+const fileTypes = ref(["JSON", "YAML", "XLSX"]);
+
+onMounted(() => {
+  setSearchFields();
+
+  fetchUnique();
+});
+
+const plans = computed(() => store.getters["plans/all"]);
+const total = computed(() => store.getters["plans/total"]);
+const isLoading = computed(() => store.getters["plans/loading"]);
+
+const filter = computed(() => store.getters["appSearch/filter"]);
+const param = computed(() => store.getters["appSearch/param"]);
+
+const filters = computed(() => {
+  const filters = Object.keys(filter.value).reduce((newFilter, key) => {
+    if (filter.value[key] === undefined || filter.value[key] === null) {
+      delete newFilter[key];
+    } else {
+      newFilter[key] = filter.value[key];
+    }
+
+    return newFilter;
+  }, {});
+
+  for (const key of Object.keys(customParams.value.filters || {})) {
+    filters[key] = customParams.value.filters[key];
+  }
+
+  if (param.value || filters.title) {
+    filters.search_param = param.value || filters.title;
+  }
+
+  return filters;
+});
+
+const requestOptions = computed(() => ({
+  ...customParams.value,
+  filters: filters.value,
+  page: page.value,
+  limit: options.value.itemsPerPage,
+  field: options.value.sortBy?.[0],
+  sort:
+    options.value.sortBy?.[0] && options.value.sortDesc?.[0] ? "DESC" : "ASC",
+}));
+
+const searchFields = computed(() => [
+  {
+    title: "Title",
+    key: "title",
+    type: "input",
+  },
+  {
+    items: Object.keys(PlanKind)
+      .filter((value) => !Number.isInteger(+value))
+      .map((key) => ({
+        text: key,
+        value: PlanKind[key],
+      })),
+    title: "Kind",
+    key: "kind",
+    type: "select",
+  },
+  { items: [], title: "Type", key: "type", type: "select" },
+  {
+    items: Object.keys(NoCloudStatus)
+      .filter((value) => !Number.isInteger(+value))
+      .map((key) => ({
+        text: key,
+        value: NoCloudStatus[key],
+      })),
+    title: "Status",
+    key: "status",
+    type: "select",
+  },
+  {
+    title: "Public",
+    key: "public",
+    type: "select",
+    items: [
+      { text: "Enabled", value: true },
+      { text: "Disabled", value: false },
+    ],
+  },
+]);
+
+const uploadedPlansText = computed(
+  () =>
+    "Uploaded plans:<br/>" +
+    uploadedPlans.value.map((p) => p.title).join("<br/>")
+);
+const isJson = computed(() => selectedFileType.value === "JSON");
+
+const fetchPlans = async () => {
+  fetchError.value = "";
+  try {
+    await store.dispatch("plans/fetch", requestOptions.value);
+  } catch (e) {
+    fetchError.value = e.message;
+  }
+};
+
+const fetchPlansDebounced = debounce(fetchPlans, 100);
+
+const fetchInstancesCount = async () => {
+  isInstanceCountLoading.value = true;
+  try {
+    instanceCountMap.value = (
+      await store.getters["plans/plansClient"].listPlansInstances({
+        uuids: plans.value.map((p) => p.uuid),
+      })
+    ).plans;
+  } finally {
+    isInstanceCountLoading.value = false;
+  }
+};
+
+const fetchUnique = async () => {
+  unique.value = await store.getters["plans/plansClient"].plansUnique();
+};
+
+const setOptions = (newOptions) => {
+  page.value = newOptions.page;
+  if (JSON.stringify(newOptions) !== JSON.stringify(options.value)) {
+    options.value = newOptions;
+  }
+};
+
+const updatePlan = async (item, { key, value }) => {
+  try {
+    updatedPlanUuid.value = item.uuid;
+
+    await store.getters["plans/plansClient"].updatePlan(
+      Plan.fromJson({
+        uuid: item.uuid,
+        [key]: value,
+      })
+    );
+    item[key] = value;
+  } catch {
+    store.commit("snackbar/showSnackbarError", {
+      message: "Error during update plan",
+    });
+  } finally {
+    updatedPlanUuid.value = "";
+  }
+};
+
+const getStatus = (item) => {
+  return statusMap[item.status] || statusMap.UNSPECIFIED;
+};
+
+const isDeleted = (plan) => {
+  return plan.status === "DEL";
+};
+
+const setSearchFields = () => {
+  store.commit("appSearch/setFields", searchFields.value);
+};
+
+const downloadXlsx = () => {
+  return downloadPlanXlsx(value.value);
+};
+
+const onJsonInputChange = async (file) => {
+  uploadedPlans.value = [];
+  try {
+    const data = isJson.value
+      ? await readJSONFile(file)
+      : await readYAMLFile(file);
+    if (Array.isArray(data)) {
+      uploadedPlans.value.push(...data);
+    } else {
+      uploadedPlans.value.push(data);
+    }
+  } catch (err) {
+    uploadedPlans.value = [];
+    store.commit("snackbar/showSnackbarError", {
+      message: err,
+    });
+  }
+};
+
+const uploadPlans = async () => {
+  isPlansUploadLoading.value = true;
+  try {
+    await Promise.all(
+      uploadedPlans.value.map((p) =>
+        store.getters["plans/plansClient"].createPlan({
+          ...p,
+          uuid: undefined,
+        })
+      )
+    );
+    fetchPlansDebounced();
+  } catch (err) {
+    store.commit("snackbar/showSnackbarError", {
+      message: err,
+    });
+  } finally {
+    uploadedPlans.value = [];
+    isPlansUploadLoading.value = false;
+  }
+};
+
+watch(searchFields, setSearchFields, { deep: true });
+
+watch(
+  filters,
+  (newVal, oldVal) => {
+    if (JSON.stringify(newVal) !== JSON.stringify(oldVal)) {
+      page.value = 1;
+      fetchPlansDebounced();
+    }
+  },
+  { deep: true }
+);
+watch(options, fetchPlansDebounced);
+watch(refetch, fetchPlansDebounced);
+
+watch(plans, () => {
+  if (plans.value.length) {
+    fetchInstancesCount();
+  }
+});
+</script>
+
+<script>
+import search from "@/mixins/search.js";
+import { NoCloudStatus } from "nocloud-proto/proto/es/statuses/statuses_pb";
+import { Plan, PlanKind } from "nocloud-proto/proto/es/billing/billing_pb";
+
+export default {
+  name: "plans-table",
+  mixins: [
+    search({
+      name: "billing-plans",
+      defaultLayout: {
+        title: "Default",
+        filter: {
+          public: [true],
+          status: [NoCloudStatus.UNSPECIFIED],
+        },
+      },
+    }),
+  ],
+};
+</script>
