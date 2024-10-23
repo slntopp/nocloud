a2d85466eec52f2d3107b9e1e4819ad932b3ec09
coddmeistr <euseew.maxim2015@yandex.ru>
2024-10-23 16:27:05 +0300

    add logs

--- cmd/billing/main.go
+++ cmd/billing/main.go
@@ -127,6 +127,7 @@ func main() {
 	invoices := graph.NewInvoicesController(log, db)
 	manager := invoices_manager.NewInvoicesManager(bClient, &invoices, authInterceptor)
 	payments.RegisterGateways(whmcsData, accounts, *manager)
+	log.Info("Payment gateways were registered")
 
 	// Register WHMCS hooks handler
 	whmcsGw := whmcs_gateway.NewWhmcsGateway(whmcsData, &accounts, manager)
