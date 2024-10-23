f1ef14f2e2411c328d859807d0e91ed8f8a9d6f8
coddmeistr <euseew.maxim2015@yandex.ru>
2024-10-23 17:23:07 +0300

    fix

--- pkg/registry/accounts.go
+++ pkg/registry/accounts.go
@@ -657,7 +657,7 @@ func (s *AccountsServiceServer) SignUp(ctx context.Context, request *accountspb.
 	if err != nil {
 		return nil, status.Error(codes.Internal, "Failed to create account via gateway. Error: "+err.Error())
 	}
-	data["whmcs_id"] = id
+	request.Data.Fields["whmcs_id"] = structpb.NewNumberValue(float64(id))
 
 	cred, err := credentials.MakeCredentials(request.Auth, log)
 	if err != nil {
