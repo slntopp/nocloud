7327d4680d6c2b8e93a7136b3697dcca017aabd2
coddmeistr <euseew.maxim2015@yandex.ru>
2024-10-24 14:58:40 +0300

    Registration Revert "add log"
    This reverts commit 0950b1541e2051d2c926b2206a322ce08f9ae7b8.

--- pkg/nocloud/payments/whmcs_gateway/gateway.go
+++ pkg/nocloud/payments/whmcs_gateway/gateway.go
@@ -53,7 +53,6 @@ func sendRequestToWhmcs[T any](method string, url string, body io.Reader) (T, er
 		return result, err
 	}
 
-	fmt.Println("Sending url from whmcs gateway: " + url)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return result, err
