7fba4e6ebbc237a3469541399b69b73881993530
coddmeistr <euseew.maxim2015@yandex.ru>
2024-10-25 16:43:18 +0300

    fix

--- pkg/nocloud/auth/auth.go
+++ pkg/nocloud/auth/auth.go
@@ -110,14 +110,16 @@ func JWT_AUTH_INTERCEPTOR(ctx context.Context, req interface{}, info *grpc.Unary
 		if probe.Anonymously {
 			return handler(ctx, req)
 		}
-	case "/nocloud.billing.CurrencyService/GetCurrencies":
-		return handler(ctx, req)
 	case "/nocloud.billing.CurrencyService/GetExchangeRates":
 		return handler(ctx, req)
 	}
 	ctx, err := JWT_AUTH_MIDDLEWARE(ctx)
-	if info.FullMethod != "/nocloud.registry.AccountsService/Token" && err != nil {
-		return nil, err
+	if info.FullMethod != "/nocloud.registry.AccountsService/Token" &&
+		info.FullMethod != "/nocloud.billing.CurrencyService/GetCurrencies" &&
+		info.FullMethod != "/nocloud.billing.AddonsService/List" {
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	go handleLogActivity(ctx)
