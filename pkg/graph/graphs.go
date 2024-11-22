28597ff3bdc4adff867873e7d13ecb07fb09a19e
coddmeistr <euseew.maxim2015@yandex.ru>
2024-11-22 12:56:20 +0300

    merge currency accounts list

--- pkg/graph/graphs.go
+++ pkg/graph/graphs.go
@@ -305,7 +305,8 @@ OPTIONS {order: "bfs", uniqueVertices: "global"}
 FILTER IS_SAME_COLLECTION(@@kind, node)
 // FILTER edge.level > 0 // TODO: ensure all edges have level
     LET perm = path.edges[0]
-	RETURN MERGE(node, { uuid: node._key, access: { level: perm.level, role: perm.role, namespace: path.vertices[-2]._key } })
+    LET currency = DOCUMENT(@@currencies, TO_STRING(node.currency.id))
+	RETURN MERGE(node, { uuid: node._key, currency: currency, access: { level: perm.level, role: perm.role, namespace: path.vertices[-2]._key } })
 `
 
 func listWithAccess[T Accessible](
