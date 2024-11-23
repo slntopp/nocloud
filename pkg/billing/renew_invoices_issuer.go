bbdf3c7c9f742d745165c577157a2b0db3f2f346
coddmeistr <euseew.maxim2015@yandex.ru>
2024-11-23 17:20:04 +0300

    invoices issuer job

--- pkg/billing/renew_invoices_issuer.go
+++ pkg/billing/renew_invoices_issuer.go
@@ -1,6 +1,7 @@
 package billing
 
 import (
+	"connectrpc.com/connect"
 	"context"
 	"fmt"
 	"github.com/arangodb/go-driver"
@@ -13,6 +14,9 @@ import (
 	"github.com/slntopp/nocloud/pkg/graph"
 	"github.com/slntopp/nocloud/pkg/nocloud/schema"
 	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/structpb"
+	"slices"
+	"strings"
 	"time"
 )
 
@@ -27,23 +31,28 @@ func (s *BillingServiceServer) InvoiceExpiringInstancesCronJob(ctx context.Conte
 	log = log.Named("InvoicesIssuer")
 	log.Info("Starting Invoice Expiring Instances Cron Job")
 
-	list, err := s.services.List(ctx, schema.ROOT_ACCOUNT_KEY, &services.ListRequest{})
-	if err != nil {
-		log.Error("Error getting services", zap.Error(err))
-		return
-	}
+	var (
+		errs     int
+		warns    int
+		gotPanic bool
+		started  bool
+		preErrs  int
+		preWarns int
+		created  int
+	)
 
-	log.Debug("Got list of services", zap.Any("count", len(list.Result)))
+	accountPools := map[string]*accountPool{}
+	invoices := make([]*graph.Invoice, 0)
+	const days15 = int64(3600 * 24 * 15)
+	const days10 = int64(3600 * 24 * 10)
 	invConf := MakeInvoicesConf(ctx, log, &s.settingsClient)
-
+	currConf := MakeCurrencyConf(ctx, log, &s.settingsClient)
 	var expiringPercentage = 0.9
 	if invConf.IssueRenewalInvoiceAfter > 0 && invConf.IssueRenewalInvoiceAfter <= 1 {
 		expiringPercentage = invConf.IssueRenewalInvoiceAfter
 	} else {
 		log.Warn("Wrong expiring percentage in config. Using default value", zap.Float64("percentage", expiringPercentage))
 	}
-	const days15 = int64(3600 * 24 * 15)
-	const days10 = int64(3600 * 24 * 10)
 	isExpiring := func(now, expiringAt, period int64) bool {
 		if period > days15 {
 			return (expiringAt - days10) < now
@@ -51,7 +60,14 @@ func (s *BillingServiceServer) InvoiceExpiringInstancesCronJob(ctx context.Conte
 		return (expiringAt - int64(float64(period)*(1.0-expiringPercentage))) < now
 	}
 
-	accountPools := map[string]*accountPool{}
+	list, err := s.services.List(ctx, schema.ROOT_ACCOUNT_KEY, &services.ListRequest{})
+	if err != nil {
+		log.Error("Error getting services", zap.Error(err))
+		preErrs++
+		goto end
+	}
+	log.Debug("Got list of services", zap.Any("count", len(list.Result)))
+
 	for _, srv := range list.Result {
 		for _, ig := range srv.GetInstancesGroups() {
 			for _, inst := range ig.GetInstances() {
@@ -59,16 +75,19 @@ func (s *BillingServiceServer) InvoiceExpiringInstancesCronJob(ctx context.Conte
 				acc, err := s.instances.GetInstanceOwner(ctx, inst.GetUuid())
 				if err != nil {
 					log.Error("Error getting instance owner", zap.Error(err))
+					preErrs++
 					continue
 				}
 				instId := driver.NewDocumentID(schema.INSTANCES_COL, inst.GetUuid()).String()
 				spResp, err := s.instances.GetGroup(ctx, instId)
 				if err != nil {
 					log.Error("Error getting instance group and sp", zap.Error(err))
+					preErrs++
 					continue
 				}
 				if spResp.SP == nil || spResp.Group == nil {
 					log.Error("Error getting instance group and sp. Nil values", zap.Error(err))
+					preErrs++
 					continue
 				}
 				if accountPools[acc.GetUuid()] == nil {
@@ -83,11 +102,13 @@ func (s *BillingServiceServer) InvoiceExpiringInstancesCronJob(ctx context.Conte
 				client, ok := s.drivers[ig.GetType()]
 				if !ok {
 					log.Error("Driver not found", zap.String("type", ig.GetType()))
+					preErrs++
 					continue
 				}
 				expResp, err := client.GetExpiration(ctx, &dpb.GetExpirationRequest{Instance: inst, ServicesProvider: spResp.SP})
 				if err != nil {
 					log.Error("Error getting instance expirations", zap.Error(err))
+					preErrs++
 					continue
 				}
 				accountPools[acc.GetUuid()].InstExpRecords[inst.GetUuid()] = expResp.GetRecords()
@@ -95,20 +116,23 @@ func (s *BillingServiceServer) InvoiceExpiringInstancesCronJob(ctx context.Conte
 		}
 	}
 
-	invoices, err := s.invoices.List(ctx, "")
+	invoices, err = s.invoices.List(ctx, "")
 	if err != nil {
 		log.Error("Error listing invoices", zap.Error(err))
-		return
+		preErrs++
+		goto end
 	}
 	for _, inv := range invoices {
 		if inv.GetAccount() == "" {
 			log.Warn("Not linked invoice found", zap.String("uuid", inv.GetUuid()))
+			preWarns++
 			continue
 		}
 		if accountPools[inv.GetAccount()] == nil {
 			acc, err := s.accounts.Get(ctx, inv.GetAccount())
 			if err != nil {
 				log.Error("Error getting account for invoice list", zap.Error(err))
+				preErrs++
 				continue
 			}
 			accountPools[inv.GetAccount()] = &accountPool{
@@ -121,11 +145,45 @@ func (s *BillingServiceServer) InvoiceExpiringInstancesCronJob(ctx context.Conte
 		accountPools[inv.GetAccount()].Invoices = append(accountPools[inv.GetAccount()].Invoices, inv)
 	}
 
+	started = true
 	for _, pool := range accountPools {
-		s.processAccountRenewalInvoices(ctx, log, pool, isExpiring)
+		ok, _errs, _warns := s.processAccountRenewalInvoices(ctx, log, pool, isExpiring, currConf.Currency)
+		if _errs < 0 || _warns < 0 {
+			gotPanic = true
+			continue
+		}
+		errs += _errs
+		warns += _warns
+		if ok {
+			created++
+		}
 	}
 
-	log.Info("Finished Invoice Expiring Instances Cron Job")
+end:
+	// Too lazy to watch logs, so just look at final result to see if everything is ok
+	var conclusion string
+	var preConclusion string
+	if preErrs > 0 {
+		preConclusion = "ERRORS"
+	} else if preWarns > 0 {
+		preConclusion = "WARNS"
+	}
+	if !started {
+		conclusion = "DIDN'T EVEN STARTED"
+	} else if gotPanic {
+		conclusion = "PANIC"
+	} else if errs > 0 {
+		conclusion = "ERRORS"
+	} else if warns > 0 {
+		conclusion = "OK"
+	} else {
+		conclusion = "PERFECT"
+	}
+	result := fmt.Sprintf("Invoices Cron Job Result - Initialization [%s] Processing [%s]: %d errors, %d warnings, %d init_errors, %d init_warns. Created %d invoices.", preConclusion, conclusion, errs, warns, preErrs, preWarns, created)
+	if gotPanic {
+		result += " GOT PANIC IN PROCESSING!"
+	}
+	log.Info(result, zap.Int("errs", errs), zap.Int("warns", warns), zap.Bool("got_panic", gotPanic), zap.Int("init_errs", preErrs), zap.Int("init_warns", preWarns))
 }
 
 type instanceExpData struct {
@@ -134,14 +192,16 @@ type instanceExpData struct {
 	Period   int64
 }
 
-func (s *BillingServiceServer) processAccountRenewalInvoices(ctx context.Context, log *zap.Logger, data *accountPool, isExp func(now, expiringAt, period int64) bool) {
+func (s *BillingServiceServer) processAccountRenewalInvoices(ctx context.Context, log *zap.Logger, data *accountPool, isExp func(now, expiringAt, period int64) bool, defCurr *pb.Currency) (created bool, errCount int, warnsCount int) {
 	log = log.Named("ProcessAccount").With(zap.String("account", data.Account.GetUuid()))
 	log.Info("Processing account", zap.Int("instance_count", len(data.Instances)), zap.Int("invoice_count", len(data.Invoices)), zap.Int("expirings", len(data.InstExpRecords)))
-	defer func() {
+	defer func(errs *int, warns *int) {
 		if err := recover(); err != nil {
 			log.Error("Recovered from panic", zap.Error(fmt.Errorf("%v", err)))
+			*errs = -1
+			*warns = -1
 		}
-	}()
+	}(&errCount, &warnsCount)
 
 	expData := make([]*instanceExpData, 0)
 	instances := filterInstances(data.Instances)
@@ -149,6 +209,7 @@ func (s *BillingServiceServer) processAccountRenewalInvoices(ctx context.Context
 		product, ok := inst.GetBillingPlan().GetProducts()[inst.GetProduct()]
 		if !ok {
 			log.Warn("Product not found for instance", zap.String("product", inst.GetProduct()), zap.String("instance", inst.GetUuid()))
+			warnsCount++
 			continue
 		}
 		var expires int64
@@ -162,10 +223,12 @@ func (s *BillingServiceServer) processAccountRenewalInvoices(ctx context.Context
 		}
 		if expires == 0 {
 			log.Warn("No expiration record found for instance product", zap.String("product", inst.GetProduct()), zap.String("instance", inst.GetUuid()))
+			warnsCount++
 			continue
 		}
 		if product.GetPeriod() != period {
 			log.Error("Product period mismatch", zap.String("product", inst.GetProduct()), zap.String("instance", inst.GetUuid()))
+			errCount++
 			continue
 		}
 		if !isExp(time.Now().Unix(), expires, period) {
@@ -195,6 +258,150 @@ func (s *BillingServiceServer) processAccountRenewalInvoices(ctx context.Context
 		})
 	}
 
+	ctx = context.WithValue(ctx, "create_as_draft", true)
+	if err := s.createRenewalInvoice(ctx, log, &data.Account, expData, defCurr); err != nil {
+		log.Error("Error creating renewal invoice", zap.Error(err))
+		errCount++
+		return false, errCount, warnsCount
+	}
+
+	return true, errCount, warnsCount
+}
+
+func (s *BillingServiceServer) createRenewalInvoice(ctx context.Context, log *zap.Logger, _acc *graph.Account, data []*instanceExpData, defCurr *pb.Currency) error {
+	now := time.Now().Unix()
+	fDateNum := func(d int) string {
+		if d < 10 {
+			return fmt.Sprintf("0%d", d)
+		}
+		return fmt.Sprintf("%d", d)
+	}
+
+	acc, err := s.accounts.GetAccountOrOwnerAccountIfPresent(ctx, _acc.GetUuid())
+	if err != nil {
+		log.Error("Error getting instance owner when getting subaccount", zap.Error(err))
+		return fmt.Errorf("error getting instance owner: %w", err)
+	}
+
+	if acc.Currency == nil {
+		acc.Currency = defCurr
+	}
+	rate, _, err := s.currencies.GetExchangeRate(ctx, defCurr, acc.Currency)
+	if err != nil {
+		log.Error("Error getting exchange rate", zap.Error(err))
+		return fmt.Errorf("error getting exchange rate: %w", err)
+	}
+
+	tax := acc.GetTaxRate()
+
+	inv := &graph.Invoice{
+		Invoice: &pb.Invoice{
+			Status:   pb.BillingStatus_UNPAID,
+			Items:    []*pb.Item{},
+			Type:     pb.ActionType_INSTANCE_RENEWAL,
+			Created:  now,
+			Account:  acc.GetUuid(),
+			Currency: acc.Currency,
+			Meta: map[string]*structpb.Value{
+				"creator":               structpb.NewStringValue(schema.ROOT_ACCOUNT_KEY),
+				graph.InvoiceTaxMetaKey: structpb.NewNumberValue(tax),
+			},
+		},
+	}
+
+	var (
+		total           float64
+		totalNoDiscount float64
+		expirations     = make([]int64, 0)
+	)
+	const monthSecs = 3600 * 24 * 30
+	const daySecs = 3600 * 24
+	billingData := graph.BillingData{
+		RenewalData: make(map[string]graph.RenewalData),
+	}
+	for _, d := range data {
+		inst := d.Instance
+		initCost, _ := s.instances.CalculateInstanceEstimatePrice(inst, false)
+		cost, err := s.promocodes.GetDiscountPriceByInstance(inst, false)
+		if err != nil {
+			log.Error("Error calculating instance estimate price", zap.Error(err))
+			continue
+		}
+
+		cost *= rate // Convert from NCU to  account's currency
+		cost = cost + (cost * tax)
+		initCost *= rate
+		initCost = initCost + (initCost * tax)
+		total += cost
+		totalNoDiscount += initCost
+
+		expireDate := time.Unix(d.ExpireAt, 0)
+		var untilDate time.Time
+		if d.Period == monthSecs {
+			untilDate = expireDate.AddDate(0, 1, 0)
+		} else {
+			untilDate = expireDate.Add(time.Duration(d.Period) * time.Second)
+		}
+		if untilDate.Unix()-expireDate.Unix() > daySecs {
+			untilDate = untilDate.AddDate(0, 0, -1)
+		}
+
+		bp := inst.GetBillingPlan()
+		product, hasProduct := bp.GetProducts()[inst.GetProduct()]
+		if !hasProduct {
+			log.Warn("Product not found in billing plan", zap.String("product", inst.GetProduct()))
+		}
+		invoicePrefixVal, _ := bp.GetMeta()["prefix"]
+		invoicePrefix := invoicePrefixVal.GetStringValue() + " "
+		productTitle := product.GetTitle() + " "
+		renewDescription := fmt.Sprintf("%s%s(%s.%s.%d - %s.%s.%d)", invoicePrefix, productTitle,
+			fDateNum(expireDate.Day()), fDateNum(int(expireDate.Month())), expireDate.Year(),
+			fDateNum(untilDate.Day()), fDateNum(int(untilDate.Month())), untilDate.Year())
+		renewDescription = strings.TrimSpace(renewDescription)
+
+		billingData.RenewalData[inst.GetUuid()] = graph.RenewalData{
+			ExpirationTs: d.ExpireAt,
+		}
+		item := &pb.Item{
+			Description: renewDescription,
+			Amount:      1,
+			Unit:        "Pcs",
+			Price:       cost,
+			Instance:    d.Instance.GetUuid(),
+		}
+		inv.Items = append(inv.Items, item)
+		expirations = append(expirations, d.ExpireAt)
+	}
+	inv.SetBillingData(&billingData)
+
+	if len(inv.Items) == 0 {
+		return nil
+	}
+
+	slices.Sort(expirations)
+	var dueDate = expirations[len(expirations)-1]
+	if dueDate < now {
+		dueDate = now + monthSecs
+	}
+
+	inv.Total = total
+	inv.Meta["no_discount_price"] = structpb.NewStringValue(fmt.Sprintf("%.2f %s", totalNoDiscount, acc.Currency.GetTitle()))
+
+	if val, ok := ctx.Value("create_as_draft").(bool); ok && val {
+		inv.Status = pb.BillingStatus_DRAFT
+	}
+
+	resp, err := s.CreateInvoice(ctx, connect.NewRequest(&pb.CreateInvoiceRequest{
+		IsSendEmail: true,
+		Invoice:     inv.Invoice,
+	}))
+	if err != nil {
+		log.Error("Error creating invoice", zap.Error(err))
+		return fmt.Errorf("error creating invoice: %w", err)
+	}
+
+	log.Info("Created invoice", zap.String("uuid", resp.Msg.GetUuid()), zap.Int("item_count", len(inv.Items)))
+	return nil
 }
 
 func filterInvoices(invoices []*graph.Invoice) []*graph.Invoice {
