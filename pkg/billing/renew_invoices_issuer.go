d48fe7aab71e315131bef3025e43c1754af93fe7
coddmeistr <euseew.maxim2015@yandex.ru>
2024-11-25 00:18:50 +0300

    allow 0 total in renew cron

--- pkg/billing/renew_invoices_issuer.go
+++ pkg/billing/renew_invoices_issuer.go
@@ -404,8 +404,8 @@ func (s *BillingServiceServer) createRenewalInvoice(ctx context.Context, log *za
 		inv.Status = pb.BillingStatus_DRAFT
 	}
 
-	if inv.Total == 0 {
-		log.Warn("Total is 0, skipping invoice creation. May be delete 0 price products from database?")
+	if inv.Total < 0 {
+		log.Warn("Total less than 0, skipping invoice creation. Wtf?")
 		return errNothingToRenew
 	}
 	resp, err := s.CreateInvoice(ctx, connect.NewRequest(&pb.CreateInvoiceRequest{
