3150782605f3ea3e0efdbf644b1359bb96631752
coddmeistr <euseew.maxim2015@yandex.ru>
2024-12-02 05:19:05 +0300

    new publishers

--- pkg/nocloud/payments/payments.go
+++ pkg/nocloud/payments/payments.go
@@ -14,7 +14,7 @@ import (
 
 type PaymentGateway interface {
 	CreateInvoice(context.Context, *pb.Invoice, ...bool) error
-	UpdateInvoice(context.Context, *pb.Invoice, *pb.Invoice) error
+	UpdateInvoice(context.Context, *pb.Invoice) error
 	PaymentURI(context.Context, *pb.Invoice) (string, error)
 	//AddClient(types.CreateUserParams) (int, error)
 }
