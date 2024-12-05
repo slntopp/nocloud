de31495867dccc3f6219a293d52709df214c112c
coddmeistr <euseew.maxim2015@yandex.ru>
2024-12-05 17:20:31 +0300

    debug consumer to read from . queue

--- cmd/billing/main.go
+++ cmd/billing/main.go
@@ -263,6 +263,7 @@ func main() {
 	go server.ConsumeInvoiceStatusActions(log, ctx, ps)
 	go server.ConsumeCreatedInstances(log, ctx, ps)
 	go server.ConsumeInvoicesWhmcsSync(log, ctx, ps, whmcsGw)
+	go server.DebugConsumer(log, ctx, ps)
 
 	log.Info("Check settings server")
 	if _, err = settingsClient.Get(ctx, &settingspb.GetRequest{}); err != nil {
