3d549170080097d2a13e38c9de3f1f71f8902af4
coddmeistr <euseew.maxim2015@yandex.ru>
2025-02-05 15:06:21 +0300

    reports: adjust removeDuplicates method

--- pkg/billing/cron_send_services_data.go
+++ pkg/billing/cron_send_services_data.go
@@ -409,7 +409,7 @@ func removeDuplicates(strings []string) []string {
 	unique := make(map[string]bool)
 	result := make([]string, 0)
 	for _, str := range strings {
-		if _, exists := unique[str]; !exists {
+		if _, exists := unique[str]; !exists && str != "" {
 			unique[str] = true
 			result = append(result, str)
 		}
