d826aa37739ede16066fadb901bf6fd1f60b317e
coddmeistr <euseew.maxim2015@yandex.ru>
2025-03-31 12:17:30 +0300

    fix

--- pkg/billing/cron_renew_invoices_issuer.go
+++ pkg/billing/cron_renew_invoices_issuer.go
@@ -436,7 +436,7 @@ func (s *BillingServiceServer) createRenewalInvoice(ctx context.Context, log *za
 func filterInvoices(invoices []*graph.Invoice) []*graph.Invoice {
 	var filteredInvoices []*graph.Invoice
 	for _, inv := range invoices {
-		if inv.GetStatus() == pb.BillingStatus_PAID || inv.GetStatus() == pb.BillingStatus_RETURNED {
+		if inv.GetStatus() == pb.BillingStatus_RETURNED {
 			continue
 		}
 		filteredInvoices = append(filteredInvoices, inv)
