7db3c16a1b041548f2f8c5ef5ef8c0fee13fc0b0
PavelSazykin <sazukinpavelal@gmail.com>
2025-06-12 17:16:48 +0400

    add time to change monitorings

---
+++ admin-ui/src/components/ui/dateTimePicker.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-text-field
+    :prepend-inner-icon="!editIcon ? 'mdi-calendar' : undefined"
+    :value="placeholder || value"
+    :dense="dense"
+    :clearable="clearable"
+    @input="emits('input', $event)"
+    :label="label"
+    :disabled="disabled"
+    :min="min"
+    type="datetime-local"
+    :append-icon="editIcon ? 'mdi-pencil' : undefined"
+  />
+</template>
+
+<script setup>
+import { toRefs } from "vue";
+
+const props = defineProps({
+  value: {},
+  min: {},
+  label: {},
+  time: { type: Boolean, default: false },
+  dense: { type: Boolean, default: false },
+  range: { type: Boolean, default: false },
+  disabled: { type: Boolean, default: false },
+  clearable: { type: Boolean, default: false },
+  editIcon: { type: Boolean, default: false },
+  placeholder: {},
+});
+const { value, min, label, dense } = toRefs(props);
+const emits = defineEmits(["input"]);
+</script>
+
+<style>
+.v-picker__title {
+  height: 88px !important;
+}
+</style>
