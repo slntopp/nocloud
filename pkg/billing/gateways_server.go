a9d6413310a02052fb9d19afd531fb45e79818d1
coddmeistr <euseew.maxim2015@yandex.ru>
2025-11-28 15:23:26 +0300

    fix

--- pkg/billing/gateways_server.go
+++ pkg/billing/gateways_server.go
@@ -137,13 +137,15 @@ func (s *PaymentGatewayServer) HandleViewInvoice(writer http.ResponseWriter, req
 	)
 
 	if group.HasOwnInvoiceBase && group.InvoiceParametersCustom != nil {
-		Supplier = InvoiceFromFields{
-			Name:       group.InvoiceParametersCustom.InvoiceFromFields.Name,
-			Address:    group.InvoiceParametersCustom.InvoiceFromFields.Address,
-			City:       group.InvoiceParametersCustom.InvoiceFromFields.City,
-			PostalCode: group.InvoiceParametersCustom.InvoiceFromFields.PostalCode,
-			Country:    group.InvoiceParametersCustom.InvoiceFromFields.Country,
-			TaxID:      group.InvoiceParametersCustom.InvoiceFromFields.TaxId,
+		if group.InvoiceParametersCustom.InvoiceFromFields != nil {
+			Supplier = InvoiceFromFields{
+				Name:       group.InvoiceParametersCustom.InvoiceFromFields.Name,
+				Address:    group.InvoiceParametersCustom.InvoiceFromFields.Address,
+				City:       group.InvoiceParametersCustom.InvoiceFromFields.City,
+				PostalCode: group.InvoiceParametersCustom.InvoiceFromFields.PostalCode,
+				Country:    group.InvoiceParametersCustom.InvoiceFromFields.Country,
+				TaxID:      group.InvoiceParametersCustom.InvoiceFromFields.TaxId,
+			}
 		}
 		LogoURL = group.InvoiceParametersCustom.LogoUrl
 	} else {
