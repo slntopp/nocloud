eafb41a4ff9146a6166665ba0f3290cc5c45e41c
PavelSazykin <sazukinpavelal@gmail.com>
2026-04-14 00:15:48 +0400

    fix invoices ui & add actions

--- admin-ui/src/components/account/invoices.vue
+++ admin-ui/src/components/account/invoices.vue
@@ -1,18 +1,39 @@
 <template>
-  <invoices-table
-    table-name="account-invoices"
-    no-search
-    hide-select
-    :custom-filter="{ account: [account.uuid] }"
-  />
+  <div>
+    <div class="d-flex justify-end">
+      <invoices-actions
+        :selected-invoices="selectedInvoices"
+        @input="selectedInvoices = $event"
+        @refresh="onRefresh"
+        :account-uuid="account.uuid"
+      />
+    </div>
+    <invoices-table
+      table-name="account-invoices"
+      no-search
+      v-model="selectedInvoices"
+      :custom-filter="{ account: [account.uuid] }"
+      :refetch="refetch"
+    />
+  </div>
 </template>
 
 <script setup>
-import { toRefs } from "vue";
+import { ref, toRefs } from "vue";
 import InvoicesTable from "../invoicesTable.vue";
+import InvoicesActions from "../invoicesActions.vue";
 
 const props = defineProps(["account"]);
 const { account } = toRefs(props);
+
+const selectedInvoices = ref([]);
+const refetch = ref(false);
+
+const onRefresh = () => {
+  console.log(2);
+  
+  refetch.value = !refetch.value;
+};
 </script>
 
 <script>
--- admin-ui/src/components/instance/invoices.vue
+++ admin-ui/src/components/instance/invoices.vue
@@ -1,25 +1,36 @@
 <template>
   <div class="pa-5">
-    <v-row align="center">
-      <v-col cols="3">
-        <date-picker
-          label="Next forced renew invoice"
-          :value="nextForcedRenewInvoice"
-          :clearable="false"
-          @input="nextForcedRenewInvoice = $event"
-          :disabled="isSaveLoading"
-        />
-      </v-col>
-      <v-col cols="1"
-        ><v-btn :loading="isSaveLoading" @click="saveNextForcedRenewInvoice"
-          >Save</v-btn
-        ></v-col
-      >
-    </v-row>
+    <div class="d-flex justify-between">
+      <v-row align="center">
+        <v-col cols="3">
+          <date-picker
+            label="Next forced renew invoice"
+            :value="nextForcedRenewInvoice"
+            :clearable="false"
+            @input="nextForcedRenewInvoice = $event"
+            :disabled="isSaveLoading"
+          />
+        </v-col>
+        <v-col cols="1"
+          ><v-btn :loading="isSaveLoading" @click="saveNextForcedRenewInvoice"
+            >Save</v-btn
+          ></v-col
+        >
+      </v-row>
+
+      <invoices-actions
+        :selected-invoices="selectedInvoices"
+        @input="selectedInvoices = $event"
+        @refresh="onRefresh"
+        :account-uuid="account.uuid"
+      />
+    </div>
+
     <invoices-table
       table-name="instance-invoices"
       no-search
-      hide-select
+      :refetch="refetch"
+      v-model="selectedInvoices"
       :custom-filter="{ instances: [template.uuid] }"
     />
   </div>
@@ -28,6 +39,7 @@
 <script setup>
 import { onMounted, ref, toRefs } from "vue";
 import InvoicesTable from "../invoicesTable.vue";
+import InvoicesActions from "../invoicesActions.vue";
 import DatePicker from "../ui/dateTimePicker.vue";
 import {
   formatDateToTimestamp,
@@ -36,7 +48,8 @@ import {
 import { useStore } from "@/store";
 import { UpdateRequest } from "nocloud-proto/proto/es/instances/instances_pb";
 
-const props = defineProps(["template"]);
+const props = defineProps(["template", "account"]);
+
 const { template } = toRefs(props);
 
 const store = useStore();
@@ -44,15 +57,22 @@ const store = useStore();
 const nextForcedRenewInvoice = ref();
 const isSaveLoading = ref(false);
 
+const refetch = ref(false);
+const selectedInvoices = ref([]);
+
 onMounted(() => {
   if (!+template.value?.meta?.nextForcedRenewInvoice) {
     return;
   }
   const nextDate = timestampToDateTimeLocal(
-    template.value?.meta?.nextForcedRenewInvoice || 0
+    template.value?.meta?.nextForcedRenewInvoice || 0,
   );
   nextForcedRenewInvoice.value = nextDate;
 });
+
+const onRefresh = () => {
+  refetch.value = !refetch.value;
+};
 
 const saveNextForcedRenewInvoice = async () => {
   var date = formatDateToTimestamp(new Date(nextForcedRenewInvoice.value)) || 0;
@@ -68,7 +88,7 @@ const saveNextForcedRenewInvoice = async () => {
     console.log(data.meta, date);
 
     await store.getters["instances/instancesClient"].update(
-      UpdateRequest.fromJson({ instance: data }, { ignoreUnknownFields: true })
+      UpdateRequest.fromJson({ instance: data }, { ignoreUnknownFields: true }),
     );
   } catch (e) {
     console.log(e);
