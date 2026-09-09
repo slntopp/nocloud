105580348804c95f596b7f073b165fe9faa7e55c
ALKIY <leshakiyko@gmail.com>
2026-09-09 15:07:16 +0300

    update modal deleted

--- admin-ui/src/components/service/info.vue
+++ admin-ui/src/components/service/info.vue
@@ -181,23 +181,28 @@
       </v-expansion-panels>
     </v-row>
 
-    <v-dialog persistent v-model="deleteIgDialog" max-width="480">
-      <v-card class="pa-4">
-        <v-card-title class="text-h6">Are you sure?</v-card-title>
-        <v-card-text class="pt-2">
-          {{ deleteIgWarning(pendingDeleteGroup) }}
-        </v-card-text>
-        <v-card-actions class="justify-center">
-          <v-btn @click="closeDeleteIg">Cancel</v-btn>
-          <v-btn
-            color="error"
-            class="ml-4"
-            :loading="isLoading"
-            @click="confirmDeleteIg"
-          >
-            Delete
-          </v-btn>
-        </v-card-actions>
+    <v-dialog persistent v-model="deleteIgDialog" max-width="400">
+      <v-card>
+        <div class="confirm-card">
+          <v-card-title class="text-h6">
+            Are you sure you want to do this?
+          </v-card-title>
+          <v-card-text style="white-space: break-spaces">
+            {{ deleteIgWarning(pendingDeleteGroup) }}
+          </v-card-text>
+          <v-card-actions class="buttons">
+            <v-btn color="red darken-1" :disabled="isLoading" @click="closeDeleteIg">
+              Cancel
+            </v-btn>
+            <v-btn
+              color="primary darken-1"
+              :loading="isLoading"
+              @click="confirmDeleteIg"
+            >
+              Confirm
+            </v-btn>
+          </v-card-actions>
+        </div>
       </v-card>
     </v-dialog>
 
@@ -549,5 +554,20 @@ export default {
 .instance-group-button {
   max-width: 30px;
   margin-left: 25px;
+}
+
+.confirm-card {
+  background-color: var(--v-background-base);
+  padding: 30px;
+}
+
+.confirm-card .buttons {
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+.confirm-card .buttons >>> button {
+  margin: 0 20px;
 }
 </style>
